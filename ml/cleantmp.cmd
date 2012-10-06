4c8044176de5dd8b1367af419eb5998f7c51bcbb
Josh Berdine <jjb@microsoft.com>
2012-10-07 00:31:55 +0100

    clean .z3-trace

--- ml/cleantmp.cmd
+++ ml/cleantmp.cmd
@@ -9,7 +9,7 @@ REM files produced by compile_mlapi.cmd
 del /q 2>NUL *.cmi *.cmo *.cmx *.cma *.cmxa *.obj *.lib *.pdb ocamlz3.exe
 
 REM files produced by test_mlapi.cmd
-del /q 2>NUL test*.exe queen*.exe test_*api.out test_*apiV3.out test_*api.err test_*apiV3.err queen.out queen.err z3.log ml.log test_mlapi.log
+del /q 2>NUL test*.exe queen*.exe test_*api.out test_*apiV3.out test_*api.err test_*apiV3.err queen.out queen.err z3.log ml.log test_mlapi.log .z3-trace
 
 REM backup files
 del /q 2>NUL *~
