39d6628be91d175c4d19fb2c705424306f322ae7
Leonardo de Moura <leonardo@microsoft.com>
2012-10-21 18:23:20 -0700

    checkpoint
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ src/framework/cmd_context_to_goal.h
@@ -0,0 +1,24 @@
+/*++
+Copyright (c) 2012 Microsoft Corporation
+
+Module Name:
+
+    cmd_context_to_goal.h
+
+Abstract:
+    Procedure for copying the assertions in the
+    command context to a goal object.
+
+Author:
+
+    Leonardo (leonardo) 2012-10-21
+
+Notes:
+
+--*/
+#ifndef _CMD_CONTEXT_TO_GOAL_H_
+#define _CMD_CONTEXT_TO_GOAL_H_
+
+void assert_exprs_from(cmd_context const & ctx, goal & t);
+
+#endif
---
+++ src/framework/echo_tactic.h
@@ -0,0 +1,30 @@
+/*++
+Copyright (c) 2011 Microsoft Corporation
+
+Module Name:
+
+    echo_tactic.h
+
+Abstract:
+
+    Tactic and probe for dumping data.
+
+Author:
+
+    Leonardo (leonardo) 2012-10-20
+
+Notes:
+
+--*/
+#ifndef _ECHO_TACTICS_H_
+#define _ECHO_TACTICS_H_
+
+class cmd_context;
+class tactic;
+class probe;
+
+tactic * mk_echo_tactic(cmd_context & ctx, char const * msg, bool newline = true);
+// Display the value returned by p in the diagnostic_stream
+tactic * mk_probe_value_tactic(cmd_context & ctx, char const * msg, probe * p, bool newline = true);
+
+#endif
