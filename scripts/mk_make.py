3e89fc092e774f3ea93b204611ccd61007d6ff9b
Leonardo de Moura <leonardo@microsoft.com>
2012-10-26 15:27:03 -0700

    Moved Microsoft.Z3V3 to dead folder
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- scripts/mk_make.py
+++ scripts/mk_make.py
@@ -64,8 +64,7 @@ add_exe('shell', ['api', 'sat', 'extra_cmds'], exe_name='z3')
 add_exe('test', ['api', 'fuzzing'], exe_name='test-z3')
 API_files = ['z3_api.h']
 add_dll('api_dll', ['api', 'sat', 'extra_cmds'], 'api/dll', reexports=['api'], dll_name='libz3', export_files=API_files)
-add_dot_net_dll('dotnet', ['api_dll'], 'bindings/dotnet/Microsoft.Z3', dll_name='Microsoft.Z3', assembly_info_dir='Properties')
-add_dot_net_dll('dotnetV3', ['api_dll'], 'bindings/dotnet/Microsoft.Z3V3', dll_name='Microsoft.Z3V3')
+add_dot_net_dll('dotnet', ['api_dll'], 'bindings/dotnet', dll_name='Microsoft.Z3', assembly_info_dir='Properties')
 set_python_dir('bindings/python')
 
 update_version(4, 2, 0, 0)
