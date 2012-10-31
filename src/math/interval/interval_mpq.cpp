81193fd550b419ce517776cc3dd6865efa119fb9
Leonardo de Moura <leonardo@microsoft.com>
2012-10-31 11:16:43 -0700

    add default template instance
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ src/math/interval/interval_mpq.cpp
@@ -0,0 +1,21 @@
+/*++
+Copyright (c) 2012 Microsoft Corporation
+
+Module Name:
+
+    interval_mpq.cpp
+
+Abstract:
+
+    Instantiate template using defaults.
+
+Author:
+
+    Leonardo de Moura (leonardo) 2012-10-31.
+
+Revision History:
+
+--*/
+#include"interval_def.h"
+
+template class interval_manager<im_default_config>;
