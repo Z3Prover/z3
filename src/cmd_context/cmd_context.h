9754ccf8a14750eded85360c96414e1736846fe0
Leonardo de Moura <leonardo@microsoft.com>
2012-12-04 11:16:42 -0800

    fixing problems with the new parameter framework
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- src/cmd_context/cmd_context.h
+++ src/cmd_context/cmd_context.h
@@ -249,6 +249,7 @@ public:
     cmd_context(bool main_ctx = true, ast_manager * m = 0, symbol const & l = symbol::null);
     ~cmd_context(); 
     context_params  & params() { return m_params; }
+    void global_params_updated(); // this method should be invoked when global (and module) params are updated.
     void set_logic(symbol const & s);
     bool has_logic() const { return m_logic != symbol::null; }
     symbol const & get_logic() const { return m_logic; }
