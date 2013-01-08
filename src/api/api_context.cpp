4ea06b8040cf307a1b274a7ea242dc1a1475febe
Leonardo de Moura <leonardo@microsoft.com>
2013-01-07 16:22:47 -0800

    Fix Z3_enable_trace API
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- src/api/api_context.cpp
+++ src/api/api_context.cpp
@@ -492,8 +492,11 @@ extern "C" {
     }
 
     void Z3_API Z3_enable_trace(Z3_string tag) {
+        memory::initialize(UINT_MAX);
         LOG_Z3_enable_trace(tag);
-        enable_trace(tag);
+        // Tag is a string that was probably not allocated by Z3. Create a copy using symbol.
+        symbol tag_sym(tag);
+        enable_trace(tag_sym.bare_str());
     }
 
     void Z3_API Z3_disable_trace(Z3_string tag) {
