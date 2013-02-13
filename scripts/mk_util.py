fa0bd4f78947ff66f25e2939aaa7e419aeb68666
Leonardo de Moura <leonardo@microsoft.com>
2013-02-13 08:16:08 -0800

    Fix git_hash function
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

--- scripts/mk_util.py
+++ scripts/mk_util.py
@@ -76,7 +76,8 @@ GPROF=False
 
 def git_hash():
     try:
-        r = subprocess.check_output(['git', 'show-ref', '--abbrev=12', 'HEAD'], shell=True).rstrip('\r\n')
+        branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], shell=True).rstrip('\r\n')
+        r = subprocess.check_output(['git', 'show-ref', '--abbrev=12', 'refs/heads/%s' % branch], shell=True).rstrip('\r\n')
     except:
         raise MKException("Failed to retrieve git hash")
     ls = r.split(' ')
