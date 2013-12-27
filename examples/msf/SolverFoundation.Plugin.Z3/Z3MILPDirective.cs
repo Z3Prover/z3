5cc4cc82264790b2bfd60068cbf1107e0f25a693
Anh-Dung Phan <t-anphan@microsoft.com>
2013-12-27 11:18:10 -0800

    Add MSF plugins

---
+++ examples/msf/SolverFoundation.Plugin.Z3/Z3MILPDirective.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.SolverFoundation.Services;
+using System;
+
+namespace Microsoft.SolverFoundation.Plugin.Z3
+{
+    public class Z3MILPDirective : Z3BaseDirective
+    {
+    }
+}
---
+++ examples/msf/SolverFoundation.Plugin.Z3/Z3TermDirective.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.SolverFoundation.Services;
+using System;
+
+namespace Microsoft.SolverFoundation.Plugin.Z3
+{
+    public class Z3TermDirective : Z3BaseDirective
+    {
+    }
+}
