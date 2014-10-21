de9f6d3e11d536e6dabf4247747214da96af3546
Christoph M. Wintersteiger <cwinter@microsoft.com>
2014-10-21 16:52:16 +0100

    FPA name clash fix
    Signed-off-by: Christoph M. Wintersteiger <cwinter@microsoft.com>

--- src/ast/float_decl_plugin.cpp
+++ src/ast/float_decl_plugin.cpp
@@ -314,8 +314,8 @@ func_decl * float_decl_plugin::mk_binary_decl(decl_kind k, unsigned num_paramete
     symbol name;
     switch (k) {
     case OP_FLOAT_REM: name = "remainder";  break;
-    case OP_FLOAT_MIN: name = "min";   break;
-    case OP_FLOAT_MAX: name = "max";   break;
+    case OP_FLOAT_MIN: name = "fp.min";   break;
+    case OP_FLOAT_MAX: name = "fp.max";   break;
     default:
         UNREACHABLE();
         break;
