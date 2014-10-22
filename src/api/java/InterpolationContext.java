4304012971ef5293a09d73b010bd3263d5b9d9a5
Christoph M. Wintersteiger <cwinter@microsoft.com>
2014-10-22 16:55:08 +0100

    Java API: copyright notices
    Signed-off-by: Christoph M. Wintersteiger <cwinter@microsoft.com>

--- src/api/java/InterpolationContext.java
+++ src/api/java/InterpolationContext.java
@@ -1,6 +1,20 @@
 /**
- * 
- */
+Copyright (c) 2012-2014 Microsoft Corporation
+   
+Module Name:
+
+   InterpolationContext.java
+
+Abstract:
+
+Author:
+
+    @author Christoph Wintersteiger (cwinter) 2012-03-15
+
+Notes:
+    
+**/ 
+
 package com.microsoft.z3;
 
 import java.util.Map;
