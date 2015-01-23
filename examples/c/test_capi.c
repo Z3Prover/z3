48c72d2c38bc0d1da9d2627e9b933bb65f4de5a6
Christoph M. Wintersteiger <cwinter@microsoft.com>
2015-01-23 18:18:26 +0000

    FPA API: naming consistency
    Signed-off-by: Christoph M. Wintersteiger <cwinter@microsoft.com>

--- examples/c/test_capi.c
+++ examples/c/test_capi.c
@@ -2665,10 +2665,10 @@ void fpa_example() {
 	c2 = Z3_mk_fpa_to_fp_bv(ctx,
 							Z3_mk_numeral(ctx, "4619567317775286272", Z3_mk_bv_sort(ctx, 64)),
 							Z3_mk_fpa_sort(ctx, 11, 53));
-	c3 = Z3_mk_fpa_to_fp_real_int(ctx,
+	c3 = Z3_mk_fpa_to_fp_int_real(ctx,
                                   Z3_mk_fpa_rtz(ctx),
-                                  Z3_mk_numeral(ctx, "1.75", Z3_mk_real_sort(ctx)),
-                                  Z3_mk_numeral(ctx, "2", Z3_mk_int_sort(ctx)),
+								  Z3_mk_numeral(ctx, "2", Z3_mk_int_sort(ctx)),
+                                  Z3_mk_numeral(ctx, "1.75", Z3_mk_real_sort(ctx)),                                  
                                   Z3_mk_fpa_sort(ctx, 11, 53));
 	c4 = Z3_mk_fpa_to_fp_real(ctx,
 							  Z3_mk_fpa_rtz(ctx),
