088dc411bab7e65d99180245f94f0afd71fb9737
Christoph M. Wintersteiger <cwinter@microsoft.com>
2015-02-05 18:32:36 +0000

    fixed potential handle leak
    Signed-off-by: Christoph M. Wintersteiger <cwinter@microsoft.com>

--- src/shell/smtlib_frontend.cpp
+++ src/shell/smtlib_frontend.cpp
@@ -124,6 +124,7 @@ unsigned read_smtlib2_commands(char const * file_name) {
             exit(ERR_OPEN_FILE);
         }
         result = parse_smt2_commands(ctx, in);
+        in.close();
     }
     else {
         result = parse_smt2_commands(ctx, std::cin, true);
