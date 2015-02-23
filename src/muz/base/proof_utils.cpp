fcb4962016bab1064dd98ff094a5227346fe6951
nikolajbjorner <nbjorner@microsoft.com>
2015-02-23 11:18:24 -0800

    add patch suggested by Arie Gurfinkel
    Signed-off-by: nikolajbjorner <nbjorner@microsoft.com>

--- src/muz/base/proof_utils.cpp
+++ src/muz/base/proof_utils.cpp
@@ -156,12 +156,22 @@ public:
             SASSERT(m.get_num_parents(p) == 1);
             tmp = m.get_parent(p, 0);
             elim(tmp);
-            get_literals(m.get_fact(p));
             expr_set* hyps = m_hypmap.find(tmp);
             expr_set* new_hyps = 0;
             if (hyps) {
                 new_hyps = alloc(expr_set, *hyps);
             }
+            expr* fact = m.get_fact(p);
+            // when hypothesis is a single literal of the form 
+            // (or A B), and the fact of p is (or A B).            
+            if (hyps && hyps->size() == 1 && in_hypotheses(fact, hyps)) {
+                m_literals.reset();
+                m_literals.push_back(fact);
+            }
+            else {
+                get_literals(fact);
+            }
+
             for (unsigned i = 0; i < m_literals.size(); ++i) {
                 expr* e = m_literals[i];
                 if (!in_hypotheses(e, hyps)) {
