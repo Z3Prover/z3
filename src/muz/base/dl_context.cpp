03034e7b33fbd58b8b30cb76d2269cb8b1da6529
Nikolaj Bjorner <nbjorner@microsoft.com>
2015-06-23 18:57:16 +0200

    disable bottom-up COI on rules with negated predicates. Fixes issue #140
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/muz/base/dl_context.cpp
+++ src/muz/base/dl_context.cpp
@@ -298,6 +298,7 @@ namespace datalog {
     bool context::explanations_on_relation_level() const { return m_params->datalog_explanations_on_relation_level(); }
     bool context::magic_sets_for_queries() const { return m_params->datalog_magic_sets_for_queries();  }
     symbol context::tab_selection() const { return m_params->tab_selection(); }
+    bool context::xform_coi() const { return m_params->xform_coi(); }
     bool context::xform_slice() const { return m_params->xform_slice(); }
     bool context::xform_bit_blast() const { return m_params->xform_bit_blast(); }    
     bool context::karr() const { return m_params->xform_karr(); }
