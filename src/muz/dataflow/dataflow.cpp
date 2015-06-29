769127d531aee87172037e15ac121f998371eafb
Nikolaj Bjorner <nbjorner@microsoft.com>
2015-06-29 15:10:38 -0700

    add dummy file to fix build
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

---
+++ src/muz/dataflow/dataflow.cpp
@@ -0,0 +1,25 @@
+/*++
+Copyright (c) 2015 Microsoft Corporation
+
+Module Name:
+
+    dataflow.cpp
+
+Abstract:
+
+    Generic bottom-up and top-down data-flow engine for analysis
+    of rule sets.
+
+Author:
+    Henning Guenther (t-hennig)
+
+--*/
+
+#include "dataflow.h"
+
+namespace datalog {
+
+    void dummy_dataflow() {
+        
+    }
+}
