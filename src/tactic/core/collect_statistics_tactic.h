b54ef3623b796586af7f2915077946409d1734e3
Christoph M. Wintersteiger <cwinter@microsoft.com>
2016-06-03 20:26:05 +0100

    added collect-statistics tactic

---
+++ src/tactic/core/collect_statistics_tactic.h
@@ -0,0 +1,33 @@
+/*++
+Copyright (c) 2016 Microsoft Corporation
+
+Module Name:
+
+    collect_statistics_tactic.h
+
+Abstract:
+
+    A tactic for collection of various statistics. 
+
+Author:
+
+    Mikolas Janota (mikjan) 2016-06-03
+    Christoph (cwinter) 2016-06-03
+
+Notes:
+
+--*/
+#ifndef COLLECT_STATISTICS_H_
+#define COLLECT_STATISTICS_H_
+
+#include"params.h"
+class ast_manager;
+class tactic;
+
+tactic * mk_collect_statistics_tactic(ast_manager & m, params_ref const & p = params_ref());
+/*
+  ADD_TACTIC("collect-statistics", "Collects various statistics.", "mk_collect_statistics_tactic(m, p)")
+*/
+
+
+#endif
