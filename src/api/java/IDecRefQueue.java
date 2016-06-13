b65d83aacfea8adacebe7d2919c55bdedb475a8c
George Karpenkov <george@metaworld.me>
2016-06-13 12:22:32 +0200

    Java API: explain the phantom references mechanics in Javadoc.

--- src/api/java/IDecRefQueue.java
+++ src/api/java/IDecRefQueue.java
@@ -24,8 +24,17 @@ import java.util.IdentityHashMap;
 import java.util.Map;
 
 /**
+ * A queue to handle management of native memory.
  *
- * @param <T>
+ * <p><b>Mechanics: </b>once an object is created, a metadata is stored for it in
+ * {@code referenceMap}, and a {@link PhantomReference} is created with a
+ * reference  to {@code referenceQueue}.
+ * Once the object becomes strongly unreachable, the phantom reference gets
+ * added by JVM to the {@code referenceQueue}.
+ * After each object creation, we iterate through the available objects in
+ * {@code referenceQueue} and decrement references for them.
+ *
+ * @param <T> Type of object stored in queue.
  */
 public abstract class IDecRefQueue<T extends Z3Object> {
     private final ReferenceQueue<T> referenceQueue = new ReferenceQueue<>();
