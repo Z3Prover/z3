d495b086395ac8693151e0561938c3fddfbb1d76
Christoph M. Wintersteiger <cwinter@microsoft.com>
2016-10-05 15:34:02 +0100

    Build/test fix for python3

--- src/api/python/z3/z3num.py
+++ src/api/python/z3/z3num.py
@@ -10,6 +10,8 @@ from .z3core import *
 from .z3printer import *
 from fractions import Fraction
 
+from .z3 import _get_ctx
+
 def _to_numeral(num, ctx=None):
     if isinstance(num, Numeral):
         return num
