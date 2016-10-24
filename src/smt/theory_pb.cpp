33e7dccd428ab194533811951a1412b49e1e44b1
Nikolaj Bjorner <nbjorner@microsoft.com>
2016-10-24 09:11:02 -0700

    merge
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/smt/theory_pb.cpp
+++ src/smt/theory_pb.cpp
@@ -1292,8 +1292,8 @@ namespace smt {
         IF_VERBOSE(1, verbose_stream() 
                    << "(smt.pb compile sorting network bound: " 
                    << k << " literals: " << in.size() 
-                   << " clauses: " << num_compiled_clauses 
-                   << " vars: " << num_compiled_vars << ")\n";);
+                   << " clauses: " << sortnw.m_stats.m_num_compiled_clauses 
+                   << " vars: " << sortnw.m_stats.m_num_compiled_vars << ")\n";);
 
         // auxiliary clauses get removed when popping scopes.
         // we have to recompile the circuit after back-tracking.
