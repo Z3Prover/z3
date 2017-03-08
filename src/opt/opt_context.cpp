829519b83747992056953cd4d5a69163982e4aac
Nikolaj Bjorner <nbjorner@microsoft.com>
2017-03-08 10:19:35 +0100

    fix bug for bit-vector optimization. Issue #928
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/opt/opt_context.cpp
+++ src/opt/opt_context.cpp
@@ -1002,8 +1002,8 @@ namespace opt {
             TRACE("opt", tout << "Term does not evaluate " << term << "\n";);
             return false;
         }
-        unsigned bv_size;
-        if (!m_arith.is_numeral(val, r) && !m_bv.is_numeral(val, r, bv_size)) {
+        unsigned bvsz;
+        if (!m_arith.is_numeral(val, r) && !m_bv.is_numeral(val, r, bvsz)) {
             TRACE("opt", tout << "model does not evaluate objective to a value\n";);
             return false;
         }
