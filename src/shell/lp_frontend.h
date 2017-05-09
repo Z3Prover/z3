561522f882f6467864f25ae65b8d8a9136b89586
Nikolaj Bjorner <nbjorner@microsoft.com>
2017-05-09 14:20:13 -0700

    missing file
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

---
+++ src/shell/lp_frontend.h
@@ -0,0 +1,8 @@
+/*
+  Copyright (c) 2013 Microsoft Corporation. All rights reserved.
+  Released under Apache 2.0 license as described in the file LICENSE.
+
+  Author: Lev Nachmanson
+*/
+#pragma once
+unsigned read_mps_file(char const * mps_file_name);
