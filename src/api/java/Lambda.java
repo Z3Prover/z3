9a0981422a4277170a2d1b4758a7e772fcce94dc
Nikolaj Bjorner <nbjorner@microsoft.com>
2017-09-10 19:21:26 +0300

    lambda
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

---
+++ src/api/java/Lambda.java
@@ -0,0 +1,137 @@
+﻿/*++
+Copyright (c) 2017 Microsoft Corporation
+
+Module Name:
+
+    Lambda.java
+
+Abstract:
+
+    Z3 Java API: Lambda
+
+Author:
+
+    Christoph Wintersteiger (cwinter) 2012-03-19
+
+Notes:
+
+--*/
+
+package com.microsoft.z3;
+
+import com.microsoft.z3.enumerations.Z3_ast_kind;
+
+
+/**
+ * Lambda expressions.
+*/public class Lambda : ArrayExpr
+{
+
+    /**
+     * The number of bound variables.
+     **/
+    public int getNumBound()
+    {
+        return Native.getQuantifierNumBound(getContext().nCtx(), getNativeObject());
+    }
+
+    /**
+     * The symbols for the bound variables.
+     * 
+     * @throws Z3Exception
+     **/
+    public Symbol[] getBoundVariableNames()
+    {
+        int n = getNumBound();
+        Symbol[] res = new Symbol[n];
+        for (int i = 0; i < n; i++)
+            res[i] = Symbol.create(getContext(), Native.getQuantifierBoundName(
+                    getContext().nCtx(), getNativeObject(), i));
+        return res;
+    }
+
+    /**
+     * The sorts of the bound variables.
+     * 
+     * @throws Z3Exception
+     **/
+    public Sort[] getBoundVariableSorts()
+    {
+        int n = getNumBound();
+        Sort[] res = new Sort[n];
+        for (int i = 0; i < n; i++)
+            res[i] = Sort.create(getContext(), Native.getQuantifierBoundSort(
+                    getContext().nCtx(), getNativeObject(), i));
+        return res;
+    }
+
+    /**
+     * The body of the quantifier.
+     * 
+     * @throws Z3Exception
+     **/
+    public BoolExpr getBody()
+    {
+        return new BoolExpr(getContext(), Native.getQuantifierBody(getContext()
+                .nCtx(), getNativeObject()));
+    }
+
+
+    /**
+     * Translates (copies) the quantifier to the Context {@code ctx}.
+     * 
+     * @param ctx A context
+     * 
+     * @return A copy of the quantifier which is associated with {@code ctx}
+     * @throws Z3Exception on error
+     **/
+    public Lambda translate(Context ctx)
+    {
+        return (Lambda) super.translate(ctx);
+    }
+    
+       
+   pulic static Lambda of(Context ctx, Sort[] sorts, Symbol[] names, Expr body)
+       : base(ctx, IntPtr.Zero)
+   {
+        ctx.checkContextMatch(sorts);
+        ctx.checkContextMatch(names);
+        ctx.checkContextMatch(body);
+
+        if (sorts.length != names.length) {
+            throw new Z3Exception(
+                    "Number of sorts does not match number of names");
+
+       
+	    NativeObject = Native.mkLambda(ctx.nCtx(), 
+					  AST.arrayLength(sorts), AST.arrayToNative(sorts),
+					  Symbol.arrayToNative(names),
+					   body.getNativeObject());
+       
+   }
+
+    /**
+     * @param ctx Context to create the lambda on.
+     * @param bound Bound variables.
+     * @param body Body of the lambda expression.
+     */
+
+   internal Lambda(Context ctx, Expr[] bound, Expr body)
+       : base(ctx, IntPtr.Zero)      { 
+        ctx.checkContextMatch(body);
+
+              
+       NativeObject = Native.mkLambdaConst(ctx.nCtx(),
+					   AST.arrayLength(bound), AST.arrayToNative(bound),
+					   body.getNativeObject());
+   }
+
+
+   internal Lambda(Context ctx, IntPtr obj) : base(ctx, obj) { Contract.Requires(ctx != null); }
+
+    Lambda(Context ctx, long obj)
+    {
+        super(ctx, obj);
+    }
+
+}
