5bcdea1ae501d9f7c6ff3759d4a9347ea7075d70
Dan Liew <daniel.liew@imperial.ac.uk>
2017-10-06 20:16:09 +0100

    [TravisCI] For ASan/LSan use larger context so we get larger stack traces if needed.

--- contrib/ci/scripts/sanitizer_env.sh
+++ contrib/ci/scripts/sanitizer_env.sh
@@ -8,7 +8,7 @@
 if [ "X${ASAN_BUILD}" = "X1" ]; then
   # Use suppression files
   export LSAN_OPTIONS="print_suppressions=1,suppressions=${Z3_SRC_DIR}/contrib/suppressions/sanitizers/lsan.txt"
-  export ASAN_OPTIONS="print_suppressions=1,suppressions=${Z3_SRC_DIR}/contrib/suppressions/sanitizers/asan.txt"
+  export ASAN_OPTIONS="malloc_context_size=100,print_suppressions=1,suppressions=${Z3_SRC_DIR}/contrib/suppressions/sanitizers/asan.txt"
   : ${ASAN_SYMBOLIZER_PATH?"ASAN_SYMBOLIZER_PATH must be specified"}
 fi
 
