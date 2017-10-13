c12439fe1e3fb93a77e42e97780544a92206e011
Nikolaj Bjorner <nbjorner@microsoft.com>
2017-10-13 07:29:16 -0700

    fix #1306
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/sat/sat_solver.cpp
+++ src/sat/sat_solver.cpp
@@ -726,7 +726,7 @@ namespace sat {
         SASSERT(scope_lvl() == 0);
         if (m_config.m_dimacs_display) {
             display_dimacs(std::cout);
-            for (unsigned i = 0; i < num_lits; ++lits) {
+            for (unsigned i = 0; i < num_lits; ++i) {
                 std::cout << dimacs_lit(lits[i]) << " 0\n";
             }
             return l_undef;
