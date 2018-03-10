0ce200144923fe5856054bcad11c9f5471f0d791
Nikolaj Bjorner <nbjorner@microsoft.com>
2018-03-10 11:39:22 -0800

    fix build
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- scripts/mk_util.py
+++ scripts/mk_util.py
@@ -889,8 +889,13 @@ def is_CXX_gpp():
     return is_compiler(CXX, 'g++')
 
 def is_clang_in_gpp_form(cc):
-    version_string = check_output([cc, '--version']).encode('utf-8').decode('utf-8')
-    return version_string.find('clang') != -1
+    str = check_output([cc, '--version'])
+    try:
+       version_string = str.encode('utf-8')
+    except:
+       version_string = str
+    clang = 'clang'.encode('utf-8')
+    return version_string.find(clang) != -1
 
 def is_CXX_clangpp():
     if is_compiler(CXX, 'g++'):
