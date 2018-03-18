b82214ef0de966bd919ef1d58ba461dfa50a1953
Nikolaj Bjorner <nbjorner@microsoft.com>
2018-03-18 16:59:41 -0700

    try paths to vsvars
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- vsts.cmd
+++ vsts.cmd
@@ -1,5 +1,7 @@
 md build
 cd build
 set
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+%VS140COMNTOOLS%\vcvars64.bat
 cmake -G "NMake Makefiles" ../
 nmake
