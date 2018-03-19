8602c52bc93045133880ee547cba344223949665
Nikolaj Bjorner <nbjorner@microsoft.com>
2018-03-19 10:41:42 -0700

    fix test build
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- scripts/vsts.cmd
+++ scripts/vsts.cmd
@@ -4,3 +4,6 @@ set
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 cmake -G "NMake Makefiles" ../
 nmake
+nmake test-z3
+git pull https://github.com/z3prover/z3test z3test
+cd z3test
