ac2fe879ae4321e91d91af2e42c607e3a7e8bd8a
Nikolaj Bjorner <nbjorner@microsoft.com>
2018-03-19 15:55:00 -0700

    update script
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- scripts/vsts.cmd
+++ scripts/vsts.cmd
@@ -1,26 +1,28 @@
-rem Build
+echo "Build"
 md build
 cd build
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 cmake -DBUILD_DOTNET_BINDINGS=True -DBUILD_JAVA_BINDINGS=True -DBUILD_PYTHON_BINDINGS=True -G "NMake Makefiles" ../
 nmake
+rem TBD: test error level
 
-rem test python bindings
+echo "Test python bindings"
 pushd python
 python z3test.py z3
 python z3test.py z3num
 popd
 
-rem Build and run examples
+echo "Build and run examples"
 
 
 
-rem Build and run unit tests
+echo "Build and run unit tests"
 nmake test-z3
+rem TBD: test error level
 rem test-z3.exe -a
 
 
 cd ..
-rem Run regression tests
+echo "Run regression tests"
 rem git pull https://github.com/z3prover/z3test z3test
 rem cd z3test
