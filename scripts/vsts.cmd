a873cc41bfa56109a8280fcbcec30342f57a4567
Nikolaj Bjorner <nbjorner@microsoft.com>
2018-03-19 10:43:59 -0700

    update vsts script
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- scripts/vsts.cmd
+++ scripts/vsts.cmd
@@ -1,9 +1,15 @@
+rem Build
 md build
 cd build
-set
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 cmake -G "NMake Makefiles" ../
 nmake
+
+rem Run unit tests
 nmake test-z3
+test-z3.exe -a
+
+cd ..
+rem Run regression tests
 git pull https://github.com/z3prover/z3test z3test
 cd z3test
