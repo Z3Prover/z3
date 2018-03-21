bfc0b214ab6a17b37298e804dbe5b47966aab40c
Nikolaj Bjorner <nbjorner@microsoft.com>
2018-03-21 14:49:36 -0700

    update script
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- scripts/vsts-mac.sh
+++ scripts/vsts-mac.sh
@@ -15,5 +15,5 @@ make c_example
 
 git clone https://github.com/z3prover/z3test.git z3test
 ls
-python z3test/scripts/test_benchmarks.py ./z3.exe ./z3test/regressions/smt2
+python z3test/scripts/test_benchmarks.py ./z3 ./z3test/regressions/smt2
 
