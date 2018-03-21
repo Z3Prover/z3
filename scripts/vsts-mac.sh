16d2c2c506c7d7185de53241cfd79619a3bde3f2
Nikolaj Bjorner <nbjorner@microsoft.com>
2018-03-20 17:06:54 -0700

    mac
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- scripts/vsts-mac.sh
+++ scripts/vsts-mac.sh
@@ -2,7 +2,7 @@
 
 cd ..
 mkdir build
-CSC=/usr/bin/csc GACUTIL=/usr/bin/gacutil CXX=clang++ CC=clang python scripts/mk_make.py -x --dotnet --java --python
+CSC=/usr/bin/csc GACUTIL=/usr/bin/gacutil CXX=clang++ CC=clang python scripts/mk_make.py --dotnet --java --python
 cd build
 make
 
