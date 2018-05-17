09db72cd3601727659d204b4f3a3fe525830c353
Lev Nachmanson <levnach@hotmail.com>
2018-05-17 15:59:38 -0700

    add var_register
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

---
+++ src/util/lp/var_register.h
@@ -0,0 +1,48 @@
+/*++
+Copyright (c) 2017 Microsoft Corporation
+
+Module Name:
+
+    <name>
+
+Abstract:
+
+    <abstract>
+
+Author:
+    Lev Nachmanson (levnach)
+
+Revision History:
+
+
+--*/
+#pragma once
+namespace lp  {
+class var_register {
+    svector<unsigned> m_local_vars_to_external;
+    std::unordered_map<unsigned, unsigned> m_external_vars_to_local;
+public:
+    unsigned register_user_var(unsigned user_var) {
+        auto t = m_external_vars_to_local.find(user_var);
+        if (t != m_external_vars_to_local.end()) {
+            return t->second;
+        }
+
+        unsigned ret = size();
+        m_external_vars_to_local[user_var] = ret;
+        m_local_vars_to_external.push_back(user_var);
+        return ret;
+    }
+
+    unsigned local_var_to_user_var(unsigned local_var) const {
+        return m_local_vars_to_external[local_var];
+    }
+    unsigned size() const {
+        return m_local_vars_to_external.size();
+    }
+    void clear() {
+        m_local_vars_to_external.clear();
+        m_external_vars_to_local.clear();
+    }
+};
+}
