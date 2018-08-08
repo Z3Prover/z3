8f9c7cb6f5a338e81af35a29bf3c75118491f11e
Lev <levnach@hotmail.com>
2018-08-08 15:17:57 +0800

    preparing niil files
    Signed-off-by: Lev <levnach@hotmail.com>

---
+++ src/util/lp/niil_solver.h
@@ -0,0 +1,37 @@
+/*++
+Copyright (c) 2017 Microsoft Corporation
+
+Module Name:
+
+    <name>
+
+Abstract:
+
+    <abstract>
+
+Author:
+    Nikolaj Bjorner (nbjorner)
+    Lev Nachmanson (levnach)
+
+Revision History:
+
+
+--*/
+#pragma once
+#include "util/vector.h"
+#include "util/lp/lp_settings.h"
+#include "util/rlimit.h"
+#include "util/params.h"
+#include "nlsat/nlsat_solver.h"
+#include "util/lp/lar_solver.h"
+namespace niil {
+// nonlinear integer incremental linear solver
+class solver {
+public:
+    struct imp;
+    imp* m_imp;
+    void add_monomial(lp::var_index v, unsigned sz, lp::var_index const* vs);
+    solver(lp::lar_solver& s, reslimit& lim, params_ref const& p);
+
+};
+}
