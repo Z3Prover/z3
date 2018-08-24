1fce8ee0b1ba420ca4b72b1b218b4bef1d349857
Lev Nachmanson <levnach@hotmail.com>
2018-08-24 19:57:03 +0800

    niil_solver basic case progress
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- src/util/lp/explanation.h
+++ src/util/lp/explanation.h
@@ -21,14 +21,17 @@ Revision History:
 namespace lp {
 class explanation {
     vector<std::pair<mpq, constraint_index>> m_explanation;
+    std::unordered_set<unsigned> m_set_of_ci;
 public:
-    void clear() { m_explanation.clear(); }
+    void clear() { m_explanation.clear(); m_set_of_ci.clear(); }
     vector<std::pair<mpq, constraint_index>>::const_iterator begin() const { return m_explanation.begin(); }
     vector<std::pair<mpq, constraint_index>>::const_iterator end() const { return m_explanation.end(); }
     void push_justification(constraint_index j, const mpq& v) {
         m_explanation.push_back(std::make_pair(v, j));
     }
     void push_justification(constraint_index j) {
+        if (m_set_of_ci.find(j) != m_set_of_ci.end()) return;
+        m_set_of_ci.insert(j);
         m_explanation.push_back(std::make_pair(one_of_type<mpq>(), j));
     }
     void reset() { m_explanation.reset(); }
