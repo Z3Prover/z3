36587e4e9132496af02ad996b1d14dc1d39c9c08
Lev <levnach@hotmail.com>
2018-10-30 12:21:01 -0700

    add a test for basic sign lemma
    Signed-off-by: Lev <levnach@hotmail.com>

--- src/util/lp/nla_solver.h
+++ src/util/lp/nla_solver.h
@@ -47,7 +47,7 @@ public:
     bool need_check();
     lbool check(lp::explanation&, lemma&);
     static void test_factorization();
-    static void test_basic_sign_lemma_with_constraints();
+    static void test_basic_sign_lemma();
     static void test_basic_lemma_for_mon_zero_from_monomial_to_factors();
     static void test_basic_lemma_for_mon_zero_from_factors_to_monomial();
     static void test_basic_lemma_for_mon_neutral_from_monomial_to_factors();
