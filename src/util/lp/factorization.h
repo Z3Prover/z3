19cfbb4701a68e3113898e84d345f3df2febf0d9
Lev <levnach@hotmail.com>
2018-10-31 15:40:07 -0700

    strengthening test cases
    Signed-off-by: Lev <levnach@hotmail.com>

--- src/util/lp/factorization.h
+++ src/util/lp/factorization.h
@@ -30,6 +30,9 @@ class factorization {
     svector<lpvar>         m_vars;
     rational               m_sign;
 public:
+    factorization(){
+        TRACE("nla_solver",);
+    }
     bool is_empty() const { return m_vars.empty(); }
     svector<lpvar> & vars() { return m_vars; }
     const svector<lpvar> & vars() const { return m_vars; }
