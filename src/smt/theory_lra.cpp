1e0e4fd952c9254fade87f58000981a63ff4d5fe
Lev <levnach@hotmail.com>
2018-11-06 16:25:39 -0800

    check m.canceled() more often
    Signed-off-by: Lev <levnach@hotmail.com>

--- src/smt/theory_lra.cpp
+++ src/smt/theory_lra.cpp
@@ -2240,7 +2240,9 @@ public:
         }
 
         lbool lbl = make_feasible();
-            
+        if (m.canceled())
+            return;
+        
         switch(lbl) {
         case l_false:
             TRACE("arith", tout << "propagation conflict\n";);
