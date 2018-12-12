b13dd989f8e79ead856788629c61fcc9d47d503c
Lev <levnach@hotmail.com>
2018-12-11 14:49:10 -1000

    add a clear() method to nla_solver, fix a bug in abs values tables, add assertions, fix newtral lemma generation
    Signed-off-by: Lev <levnach@hotmail.com>

--- src/ast/ast.cpp
+++ src/ast/ast.cpp
@@ -1914,7 +1914,7 @@ void ast_manager::delete_node(ast * n) {
         TRACE("mk_var_bug", tout << "del_ast: " << " " << n->m_ref_count << "\n";);
         TRACE("ast_delete_node", tout << mk_bounded_pp(n, *this) << "\n";);
 
-        SASSERT(m_ast_table.contains(n));
+        // SASSERT(m_ast_table.contains(n));
         m_ast_table.erase(n);
         SASSERT(!m_ast_table.contains(n));
         SASSERT(!m_debug_ref_count || !m_debug_free_indices.contains(n->m_id));
