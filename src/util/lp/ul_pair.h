dc0563a9636b3f1f60eaf98c2dba1dd0a6068b48
Lev <levnach@hotmail.com>
2018-12-12 14:57:04 -1000

    avoid adding constantly false statements to the lemma
    Signed-off-by: Lev <levnach@hotmail.com>

--- src/util/lp/ul_pair.h
+++ src/util/lp/ul_pair.h
@@ -29,7 +29,6 @@ namespace lp {
 
 enum lconstraint_kind { LE = -2, LT = -1 , GE = 2, GT = 1, EQ = 0, NE = 3 };
 
-
 inline bool kind_is_strict(lconstraint_kind kind) { return kind == LT || kind == GT;}
 
 inline std::ostream& operator<<(std::ostream& out, lconstraint_kind k) {
