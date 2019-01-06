a06bc497102cdfbf49e086b33d196e8bb38f8d54
Bruce Mitchener <bruce.mitchener@gmail.com>
2019-01-06 12:15:31 +0700

    Let str_hashtable store `const char*`.
    This removes some boilerplate const casting.

--- src/util/str_hashtable.h
+++ src/util/str_hashtable.h
@@ -28,7 +28,7 @@ struct str_hash_proc {
     unsigned operator()(char const * s) const { return string_hash(s, static_cast<unsigned>(strlen(s)), 17); } 
 };
 struct str_eq_proc { bool operator()(char const * s1, char const * s2) const { return strcmp(s1, s2) == 0; } }; 
-typedef ptr_hashtable<char, str_hash_proc, str_eq_proc> str_hashtable;
+typedef ptr_hashtable<const char, str_hash_proc, str_eq_proc> str_hashtable;
 
 #endif /* STR_HASHTABLE_H_ */
 
