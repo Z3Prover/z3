e004986e992010a58ff637a430f5a202bd9df1b2
Nikolaj Bjorner <nbjorner@microsoft.com>
2019-01-30 09:20:38 -0800

    fix z3++.h
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/api/c++/z3++.h
+++ src/api/c++/z3++.h
@@ -388,6 +388,7 @@ namespace z3 {
         template<typename T2>
         array(ast_vector_tpl<T2> const & v);
         ~array() { delete[] m_array; }
+        void resize(unsigned sz) { delete[] m_array; m_array = new T[sz]; }
         unsigned size() const { return m_size; }
         T & operator[](int i) { assert(0 <= i); assert(static_cast<unsigned>(i) < m_size); return m_array[i]; }
         T const & operator[](int i) const { assert(0 <= i); assert(static_cast<unsigned>(i) < m_size); return m_array[i]; }
@@ -2239,8 +2240,8 @@ namespace z3 {
             check_error(); 
             expr_vector result(ctx(), r);
             unsigned sz = result.size();
-            levels = array<unsigned>(sz); 
-            Z3_solver_get_levels(ctx(), m_solver, r, sz, levels.c_ptr());
+            levels.resize(sz);
+            Z3_solver_get_levels(ctx(), m_solver, r, sz, levels.ptr());
             check_error(); 
             return result; 
         }
