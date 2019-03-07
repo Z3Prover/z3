70ada9919e1c8f6d6d8cb3c6af64c71a6cad9791
Nuno Lopes <nlopes@microsoft.com>
2019-03-07 11:09:25 +0000

    cmake: fix windows build with long absolute directory names

--- scripts/mk_install_tactic_cpp.py
+++ scripts/mk_install_tactic_cpp.py
@@ -14,19 +14,22 @@ def main(args):
     logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("destination_dir", help="destination directory")
-    parser.add_argument("header_files", nargs="+",
-                        help="One or more header files to parse")
+    parser.add_argument("deps", help="file with header file names to parse")
     pargs = parser.parse_args(args)
 
     if not mk_genfile_common.check_dir_exists(pargs.destination_dir):
         return 1
 
-    if not mk_genfile_common.check_files_exist(pargs.header_files):
+    if not mk_genfile_common.check_files_exist([pargs.deps]):
         return 1
 
-    h_files_full_path = []
-    for header_file in pargs.header_files:
-        h_files_full_path.append(os.path.abspath(header_file))
+    with open(pargs.deps, 'r') as f:
+        lines = f.read().split('\n')
+        h_files_full_path = [os.path.abspath(header_file)
+                                for header_file in lines if header_file]
+
+    if not mk_genfile_common.check_files_exist(h_files_full_path):
+        return 1
 
     output = mk_genfile_common.mk_install_tactic_cpp_internal(
         h_files_full_path,
