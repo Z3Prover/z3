e4eca577f6746af778fa66200127704bad80e1ab
Nikolaj Bjorner <nbjorner@microsoft.com>
2019-03-28 07:03:04 -0700

    fix po model
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/smt/smt_context.cpp
+++ src/smt/smt_context.cpp
@@ -4453,7 +4453,6 @@ namespace smt {
         }
         recfun::util u(m);
         func_decl_ref_vector recfuns = u.get_rec_funs();
-        std::cout << recfuns << "\n";
         for (func_decl* f : recfuns) {
             auto& def = u.get_def(f);
             expr* rhs = def.get_rhs();
@@ -4475,6 +4474,7 @@ namespace smt {
             fi->set_else(bodyr);
             m_model->register_decl(f, fi);
         }
+        TRACE("model", tout << *m_model << "\n";);
     }
 
 };
