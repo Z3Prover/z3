48fcd66bb97cce892ff220f75748542b37ad5423
Lev Nachmanson <levnach@hotmail.com>
2019-04-16 14:03:09 -0700

    refactor tests from nla_solver
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- src/util/lp/nla_solver.h
+++ src/util/lp/nla_solver.h
@@ -37,25 +37,10 @@ public:
     
     solver(lp::lar_solver& s, reslimit& lim, params_ref const& p);
     ~solver();
+    inline core * get_core() { return m_core; } 
     void push();
     void pop(unsigned scopes);
     bool need_check();
     lbool check(vector<lemma>&);
-    static void test_factorization();
-    static void test_basic_sign_lemma();
-    static void test_basic_lemma_for_mon_zero_from_monomial_to_factors();
-    static void test_basic_lemma_for_mon_zero_from_factors_to_monomial();
-    static void test_basic_lemma_for_mon_neutral_from_monomial_to_factors();
-    static void test_basic_lemma_for_mon_neutral_from_factors_to_monomial();
-    static void test_basic_lemma_for_mon_neutral_from_factors_to_monomial_0();
-    static void test_basic_lemma_for_mon_neutral_from_factors_to_monomial_1();
-    static void test_order_lemma();
-    static void test_order_lemma_params(bool, int sign);
-    static void test_monotone_lemma();
-    static void test_tangent_lemma();
-    static void test_tangent_lemma_reg();
-    static void test_tangent_lemma_equiv();
-    static void s_set_column_value(lp::lar_solver&, unsigned, const rational &);
-    static void s_set_column_value(lp::lar_solver&, unsigned, const lp::impq &);
 };
 }
