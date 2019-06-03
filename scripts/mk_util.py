1155a29c6e2b42b975bf6ea64de1703464829c2a
Nikolaj Bjorner <nbjorner@microsoft.com>
2019-06-02 17:26:49 -0700

    spelling
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- scripts/mk_util.py
+++ scripts/mk_util.py
@@ -2530,7 +2530,7 @@ def mk_config():
         check_ar()
         CXX = find_cxx_compiler()
         CC  = find_c_compiler()
-        SLITEXTRAFLAGS = ''
+        SLIBEXTRAFLAGS = ''
 #       SLIBEXTRAFLAGS = '%s -Wl,-soname,libz3.so.0' % LDFLAGS
         EXE_EXT = ''
         LIB_EXT = '.a'
