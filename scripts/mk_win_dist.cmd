8f2cc6c03d94b6975278f9ab12fcdad699f8d3d3
Nikolaj Bjorner <nbjorner@microsoft.com>
2019-06-04 12:50:45 -0700

    updated dist cmd wrapper
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- scripts/mk_win_dist.cmd
+++ scripts/mk_win_dist.cmd
@@ -1,4 +1,10 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 
-python scripts\mk_win_dist.py
+python scripts\mk_win_dist.py --x64-only 
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars86.bat"
+
+python scripts\mk_win_dist.py --x86-only
+
+
 
