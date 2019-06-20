7c9d2e0d75757ed6aa535f72c7dec70d5a527fed
Nikolaj Bjorner <nbjorner@microsoft.com>
2019-06-20 22:44:03 +0200

    pydoc test
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/api/python/z3/z3.py
+++ src/api/python/z3/z3.py
@@ -1270,7 +1270,7 @@ def Distinct(*args):
     >>> simplify(Distinct(x, y, z))
     Distinct(x, y, z)
     >>> simplify(Distinct(x, y, z), blast_distinct=True)
-    And(Not(x == y), Not(x == z), Not(y == z))
+    And(Not(x == y), Not(z == x), Not(z == y))
     """
     args  = _get_args(args)
     ctx   = _ctx_from_ast_arg_list(args)
@@ -8158,7 +8158,7 @@ def simplify(a, *arguments, **keywords):
     >>> simplify((x + 1)*(y + 1), som=True)
     1 + x + y + x*y
     >>> simplify(Distinct(x, y, 1), blast_distinct=True)
-    And(Not(x == y), Not(x == 1), Not(y == 1))
+    And(Not(y == x), Not(1 == x), Not(y == 1))
     >>> simplify(And(x == 0, y == 1), elim_and=True)
     Not(Or(Not(x == 0), Not(y == 1)))
     """
