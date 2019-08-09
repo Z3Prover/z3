bf3bc1432fd33b2ea42f529f6125e8e2aba1d94e
Everett Maus <evmaus@google.com>
2019-08-08 19:17:40 -0700

    Fixing a bug.

--- scripts/build_libcxx_msan.sh
+++ scripts/build_libcxx_msan.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-cd ..
 mkdir libcxx
 cd libcxx
 # Checkout LLVM, libc++ and libc++abi
@@ -12,4 +11,4 @@ svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
 mkdir libcxx_msan && cd libcxx_msan
 cmake ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_USE_SANITIZER=Memory -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
 make cxx -j4
-cd ../build
+cd ..
