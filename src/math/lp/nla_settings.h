bf79d93d519aad538e9e4b2171cda90cd547c87d
Lev Nachmanson <levnach@hotmail.com>
2019-08-23 11:01:17 -0700

    limit the row length in horner's scheme
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- src/math/lp/nla_settings.h
+++ src/math/lp/nla_settings.h
@@ -26,11 +26,14 @@ class nla_settings {
     bool m_run_horner;
     // how often to call the horner heuristic
     unsigned m_horner_frequency;
+    unsigned m_horner_row_length_limit;
 public:
     nla_settings() : m_run_order(true),
                      m_run_tangents(true),
                      m_run_horner(true),
-                     m_horner_frequency(4) {}
+                     m_horner_frequency(4),
+                     m_horner_row_length_limit(10)
+    {}
     
     bool run_order() const { return m_run_order; }
     bool& run_order() { return m_run_order; }
@@ -42,6 +45,8 @@ public:
     bool& run_horner() { return m_run_horner; }    
 
     unsigned horner_frequency() const { return m_horner_frequency; }
-    unsigned& horner_frequency() { return m_horner_frequency; }    
+    unsigned& horner_frequency() { return m_horner_frequency; }
+    unsigned horner_row_length_limit() const { return m_horner_row_length_limit; }
+    unsigned& horner_row_length_limit() { return m_horner_row_length_limit; }    
 };
 }
