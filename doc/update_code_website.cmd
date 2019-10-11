8c8a8cee7a5f03e8ac63aafeabfabcb9cd502e8e
Nikolaj Bjorner <nbjorner@microsoft.com>
2019-10-11 13:12:02 -0700

    add build step to generate doc
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- doc/update_code_website.cmd
+++
@@ -1,4 +0,0 @@
-REM Script for updating the website containing the Z3 Code documentation.
-REM You must be inside the Microsoft network to execute this script, and 
-REM robocopy must be in your PATH.
-robocopy /S code\html \\research\root\web\external\en-us\UM\redmond\projects\z3\code
