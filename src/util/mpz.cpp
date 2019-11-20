e212159f4e941c78fc03239e0884f2f0454f581f
Nikolaj Bjorner <nbjorner@microsoft.com>
2019-11-20 15:01:04 -0800

    fix #2727
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/util/mpz.cpp
+++ src/util/mpz.cpp
@@ -72,6 +72,8 @@ inline uint64_t _trailing_zeros64(uint64_t x) {
 
 #if defined(_WINDOWS) && !defined(_M_ARM) && !defined(_M_ARM64)
 // _trailing_zeros32 already defined using intrinsics
+#elif defined(__GNUC__)
+// _trailing_zeros32 already defined using intrinsics
 #else
 inline uint32_t _trailing_zeros32(uint32_t x) {
     uint32_t r = 0;
