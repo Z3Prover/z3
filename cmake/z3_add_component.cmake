Z3Prover/z3

commit 53a01a07bdf245353bd0288fc50efb2739d3d3be

rename additional build options #2709
Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

contrib/ci/scripts/build_z3_cmake.sh
@@ -59,39 +59,39 @@ fi
 # Python bindings?
 if [ "X${PYTHON_BINDINGS}" = "X1" ]; then
   ADDITIONAL_Z3_OPTS+=( \
-    '-DBUILD_PYTHON_BINDINGS=ON' \
-    '-DINSTALL_PYTHON_BINDINGS=ON' \
+    '-DZ3_BUILD_PYTHON_BINDINGS=ON' \
+    '-DZ3_INSTALL_PYTHON_BINDINGS=ON' \
   )
 else
   ADDITIONAL_Z3_OPTS+=( \
-    '-DBUILD_PYTHON_BINDINGS=OFF' \
-    '-DINSTALL_PYTHON_BINDINGS=OFF' \
+    '-DZ3_BUILD_PYTHON_BINDINGS=OFF' \
+    '-DZ3_INSTALL_PYTHON_BINDINGS=OFF' \
   )
 fi
 
 # .NET bindings?
 if [ "X${DOTNET_BINDINGS}" = "X1" ]; then
   ADDITIONAL_Z3_OPTS+=( \
-    '-DBUILD_DOTNET_BINDINGS=ON' \
-    '-DINSTALL_DOTNET_BINDINGS=ON' \
+    '-DZ3_BUILD_DOTNET_BINDINGS=ON' \
+    '-DZ3_INSTALL_DOTNET_BINDINGS=ON' \
   )
 else
   ADDITIONAL_Z3_OPTS+=( \
-    '-DBUILD_DOTNET_BINDINGS=OFF' \
-    '-DINSTALL_DOTNET_BINDINGS=OFF' \
+    '-DZ3_BUILD_DOTNET_BINDINGS=OFF' \
+    '-DZ3_INSTALL_DOTNET_BINDINGS=OFF' \
   )
 fi
 
 # Java bindings?
 if [ "X${JAVA_BINDINGS}" = "X1" ]; then
   ADDITIONAL_Z3_OPTS+=( \
-    '-DBUILD_JAVA_BINDINGS=ON' \
-    '-DINSTALL_JAVA_BINDINGS=ON' \
+    '-DZ3_BUILD_JAVA_BINDINGS=ON' \
+    '-DZ3_INSTALL_JAVA_BINDINGS=ON' \
   )
 else
   ADDITIONAL_Z3_OPTS+=( \
-    '-DBUILD_JAVA_BINDINGS=OFF' \
-    '-DINSTALL_JAVA_BINDINGS=OFF' \
+    '-DZ3_BUILD_JAVA_BINDINGS=OFF' \
+    '-DZ3_INSTALL_JAVA_BINDINGS=OFF' \
   )
 fi
 

src/CMakeLists.txt
@@ -105,7 +105,7 @@ set (object_files "")
 foreach (component ${Z3_LIBZ3_COMPONENTS_LIST})
   list(APPEND object_files $<TARGET_OBJECTS:${component}>)
 endforeach()
-if (BUILD_LIBZ3_SHARED)
+if (Z3_BUILD_LIBZ3_SHARED)
   set(lib_type "SHARED")
 else()
   set(lib_type "STATIC")
@@ -220,11 +220,11 @@ add_subdirectory(test)
 ################################################################################
 # Z3 API bindings
 ################################################################################
-option(BUILD_PYTHON_BINDINGS "Build Python bindings for Z3" OFF)
-if (BUILD_PYTHON_BINDINGS)
-  if (NOT BUILD_LIBZ3_SHARED)
+option(Z3_BUILD_PYTHON_BINDINGS "Build Python bindings for Z3" OFF)
+if (Z3_BUILD_PYTHON_BINDINGS)
+  if (NOT Z3_BUILD_LIBZ3_SHARED)
     message(FATAL_ERROR "The python bindings will not work with a static libz3. "
-            "You either need to disable BUILD_PYTHON_BINDINGS or enable BUILD_LIBZ3_SHARED")
+            "You either need to disable Z3_BUILD_PYTHON_BINDINGS or enable Z3_BUILD_LIBZ3_SHARED")
   endif()
   add_subdirectory(api/python)
 endif()
@@ -232,11 +232,11 @@ endif()
 ################################################################################
 # .NET bindings
 ################################################################################
-option(BUILD_DOTNET_BINDINGS "Build .NET bindings for Z3" OFF)
-if (BUILD_DOTNET_BINDINGS)
-  if (NOT BUILD_LIBZ3_SHARED)
+option(Z3_BUILD_DOTNET_BINDINGS "Build .NET bindings for Z3" OFF)
+if (Z3_BUILD_DOTNET_BINDINGS)
+  if (NOT Z3_BUILD_LIBZ3_SHARED)
     message(FATAL_ERROR "The .NET bindings will not work with a static libz3. "
-      "You either need to disable BUILD_DOTNET_BINDINGS or enable BUILD_LIBZ3_SHARED")
+      "You either need to disable Z3_BUILD_DOTNET_BINDINGS or enable Z3_BUILD_LIBZ3_SHARED")
   endif()
   add_subdirectory(api/dotnet)
 endif()
@@ -244,11 +244,11 @@ endif()
 ################################################################################
 # Java bindings
 ################################################################################
-option(BUILD_JAVA_BINDINGS "Build Java bindings for Z3" OFF)
-if (BUILD_JAVA_BINDINGS)
-  if (NOT BUILD_LIBZ3_SHARED)
+option(Z3_BUILD_JAVA_BINDINGS "Build Java bindings for Z3" OFF)
+if (Z3_BUILD_JAVA_BINDINGS)
+  if (NOT Z3_BUILD_LIBZ3_SHARED)
     message(FATAL_ERROR "The Java bindings will not work with a static libz3. "
-      "You either need to disable BUILD_JAVA_BINDINGS or enable BUILD_LIBZ3_SHARED")
+      "You either need to disable Z3_BUILD_JAVA_BINDINGS or enable Z3_BUILD_LIBZ3_SHARED")
   endif()
   add_subdirectory(api/java)
 endif()

src/api/dotnet/CMakeLists.txt
@@ -173,9 +173,9 @@ DOTNET_REGISTER_LOCAL_REPOSITORY(${Z3_DOTNET_LOCALREPO_NAME} ${PROJECT_BINARY_DI
 #          the build step depends on the 'purge' target, making sure that
 #          a user will always restore the freshly-built package.
 ###############################################################################
-option(INSTALL_DOTNET_BINDINGS "Install .NET bindings when invoking install target" ON)
+option(Z3_INSTALL_DOTNET_BINDINGS "Install .NET bindings when invoking install target" ON)
 
-if(INSTALL_DOTNET_BINDINGS)
+if(Z3_INSTALL_DOTNET_BINDINGS)
     install(FILES "${PROJECT_BINARY_DIR}/Microsoft.Z3/Microsoft.Z3.${Z3_DOTNET_NUPKG_VERSION}.nupkg" DESTINATION "${CMAKE_INSTALL_LIBDIR}/z3.nuget")
     # move the local repo to the installation directory (cancel the build-time repo)
     install(CODE "include(${CMAKE_CURRENT_LIST_DIR}/../../../cmake/modules/FindDotnet.cmake)\n DOTNET_REGISTER_LOCAL_REPOSITORY(\"${Z3_DOTNET_LOCALREPO_NAME}\" \"${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}/z3.nuget\")")

src/api/java/CMakeLists.txt
@@ -210,8 +210,8 @@ add_jar(z3JavaJar
 ###############################################################################
 # Install
 ###############################################################################
-option(INSTALL_JAVA_BINDINGS "Install Java bindings when invoking install target" ON)
-if (INSTALL_JAVA_BINDINGS)
+option(Z3_INSTALL_JAVA_BINDINGS "Install Java bindings when invoking install target" ON)
+if (Z3_INSTALL_JAVA_BINDINGS)
   # Provide cache variables for the install locations that the user can change.
   # This defaults to ``/usr/local/java`` which seems to be the location for ``.jar``
   # files on Linux distributions

src/api/python/CMakeLists.txt
@@ -91,8 +91,8 @@ add_custom_target(build_z3_python_bindings
 ###############################################################################
 # Install
 ###############################################################################
-option(INSTALL_PYTHON_BINDINGS "Install Python bindings when invoking install target" ON)
-if (INSTALL_PYTHON_BINDINGS)
+option(Z3_INSTALL_PYTHON_BINDINGS "Install Python bindings when invoking install target" ON)
+if (Z3_INSTALL_PYTHON_BINDINGS)
   message(STATUS "Emitting rules to install Z3 python bindings")
   # Try to guess the installation path for the bindings
   if (NOT DEFINED CMAKE_INSTALL_PYTHON_PKG_DIR)

cmake/modules/FindDotNetToolchain.cmake
@@ -1,58 +1,0 @@
-# Tries to find a working .NET tool chain
-#
-# Once complete this will define
-# DOTNET_TOOLCHAIN_FOUND  : BOOL : System has a .NET toolchain
-# DOTNET_CSC_EXECUTABLE - STRING : Path to C# compiler
-# DOTNET_GACUTIL_EXECUTABLE - STRING : Path to gacutil
-# DOTNET_TOOLCHAIN_IS_MONO : BOOL : True if detected .NET toolchain is Mono
-# DOTNET_TOOLCHAIN_IS_WINDOWS : BOOL : True if detected .NET toolchain is native Windows
-include(FindPackageHandleStandardArgs)
-
-find_program(
-  DOTNET_CSC_EXECUTABLE
-  NAMES "csc.exe" "mcs" "dmcs"
-)
-message(STATUS "DOTNET_CSC_EXECUTABLE: \"${DOTNET_CSC_EXECUTABLE}\"")
-
-find_program(
-  DOTNET_GACUTIL_EXECUTABLE
-  NAMES "gacutil.exe" "gacutil"
-)
-message(STATUS "DOTNET_GACUTIL_EXECUTABLE: \"${DOTNET_GACUTIL_EXECUTABLE}\"")
-
-# Try to determine the tool chain vendor
-set(DOTNET_DETERMINED_VENDOR FALSE)
-if (DOTNET_CSC_EXECUTABLE)
-  execute_process(COMMAND "${DOTNET_CSC_EXECUTABLE}" "/help"
-    RESULT_VARIABLE CSC_EXIT_CODE
-    OUTPUT_VARIABLE CSC_STD_OUT
-  )
-  if (${CSC_EXIT_CODE} EQUAL 0)
-    if ("${CSC_STD_OUT}" MATCHES "^Mono[ ]+C#")
-      set(DOTNET_DETERMINED_VENDOR TRUE)
-      set(DOTNET_TOOLCHAIN_IS_MONO TRUE)
-      set(DOTNET_TOOLCHAIN_IS_WINDOWS FALSE)
-      message(STATUS ".NET toolchain is Mono")
-    elseif ("${CSC_STD_OUT}" MATCHES "^Turbo[ ]+C#")
-      set(DOTNET_DETERMINED_VENDOR TRUE)
-      set(DOTNET_TOOLCHAIN_IS_MONO TRUE)
-      set(DOTNET_TOOLCHAIN_IS_WINDOWS FALSE)
-      message(STATUS ".NET toolchain is Mono")
-    elseif ("${CSC_STD_OUT}" MATCHES "^Microsoft.+Visual[ ]+C#")
-      set(DOTNET_DETERMINED_VENDOR TRUE)
-      set(DOTNET_TOOLCHAIN_IS_MONO FALSE)
-      set(DOTNET_TOOLCHAIN_IS_WINDOWS TRUE)
-      message(STATUS ".NET toolchain is Windows native")
-    else()
-      message(STATUS ".NET toolchain is unknown: ${CSC_STD_OUT}")
-    endif()
-  endif()
-endif()
-
-# TODO: Check C# compiler works
-
-find_package_handle_standard_args(DotNetToolChain DEFAULT_MSG
-  DOTNET_CSC_EXECUTABLE
-  DOTNET_GACUTIL_EXECUTABLE
-  DOTNET_DETERMINED_VENDOR
-)

src/api/dotnet/cmake_install_gac.cmake.in
@@ -1,7 +1,7 @@
 # Install assembly to the GAC
 set(GAC_ROOT "$ENV{DESTDIR}@CMAKE_INSTALL_FULL_LIBDIR@")
 execute_process(COMMAND
-  "@DOTNET_GACUTIL_EXECUTABLE@"
+  "@Z3_DOTNET_GACUTIL_EXECUTABLE@"
   "-i"
   "@Z3_DOTNET_ASSEMBLY_DLL@"
   "-f"

src/api/dotnet/cmake_uninstall_gac.cmake.in
@@ -1,7 +1,7 @@
 # Uninstall assembly from the GAC
 set(GAC_ROOT "$ENV{DESTDIR}@CMAKE_INSTALL_FULL_LIBDIR@")
 execute_process(COMMAND
-  "@DOTNET_GACUTIL_EXECUTABLE@"
+  "@Z3_DOTNET_GACUTIL_EXECUTABLE@"
   # Note ``-us`` takes assembly file name rather than
   # ``-u`` which takes an assembly display name
   "-us"

commit 457d33fd03bb666a6667bccc273ff9b56074f94e

Align CMake option for python executable with documentation
The documentation (`README-CMake.md`) mentions `Z3_PYTHON_EXECUTABLE` as
the option which should be used to specify the python executable to be
used by Z3. Unfortunately, that variable is currently not being used in
any of the CMakeLists files and therefore ignored.

It seems to me that this is a regression introduced in
53a01a07bdf245353bd0288fc50efb2739d3d3be which updated the
`README-CMake.md` but not the CMake files in regards to this variable.

CMakeLists.txt
@@ -170,7 +170,7 @@ list(APPEND Z3_COMPONENT_CXX_DEFINES $<$<CONFIG:RelWithDebInfo>:_EXTERNAL_RELEAS
 # Find Python
 ################################################################################
 find_package(PythonInterp REQUIRED)
-message(STATUS "PYTHON_EXECUTABLE: ${PYTHON_EXECUTABLE}")
+message(STATUS "Z3_PYTHON_EXECUTABLE: ${Z3_PYTHON_EXECUTABLE}")
 
 ################################################################################
 # Target architecture detection

cmake/z3_add_component.cmake
@@ -116,7 +116,7 @@ macro(z3_add_component component_name)
     set(_full_output_file_path "${CMAKE_CURRENT_BINARY_DIR}/${_output_file}")
     message(STATUS "Adding rule to generate \"${_output_file}\"")
     add_custom_command(OUTPUT "${_output_file}"
-      COMMAND "${PYTHON_EXECUTABLE}" "${PROJECT_SOURCE_DIR}/scripts/pyg2hpp.py" "${_full_pyg_file_path}" "${CMAKE_CURRENT_BINARY_DIR}"
+      COMMAND "${Z3_PYTHON_EXECUTABLE}" "${PROJECT_SOURCE_DIR}/scripts/pyg2hpp.py" "${_full_pyg_file_path}" "${CMAKE_CURRENT_BINARY_DIR}"
       MAIN_DEPENDENCY "${_full_pyg_file_path}"
       DEPENDS "${PROJECT_SOURCE_DIR}/scripts/pyg2hpp.py"
               ${Z3_GENERATED_FILE_EXTRA_DEPENDENCIES}
@@ -269,7 +269,7 @@ macro(z3_add_install_tactic_rule)
   string(REPLACE ";" "\n" _tactic_header_files "${_tactic_header_files}")
   file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/install_tactic.deps" ${_tactic_header_files})
   add_custom_command(OUTPUT "install_tactic.cpp"
-    COMMAND "${PYTHON_EXECUTABLE}"
+    COMMAND "${Z3_PYTHON_EXECUTABLE}"
     "${PROJECT_SOURCE_DIR}/scripts/mk_install_tactic_cpp.py"
     "${CMAKE_CURRENT_BINARY_DIR}"
     "${CMAKE_CURRENT_BINARY_DIR}/install_tactic.deps"
@@ -307,7 +307,7 @@ macro(z3_add_memory_initializer_rule)
   endforeach()
 
   add_custom_command(OUTPUT "mem_initializer.cpp"
-    COMMAND "${PYTHON_EXECUTABLE}"
+    COMMAND "${Z3_PYTHON_EXECUTABLE}"
     "${PROJECT_SOURCE_DIR}/scripts/mk_mem_initializer_cpp.py"
     "${CMAKE_CURRENT_BINARY_DIR}"
     ${_mem_init_finalize_headers}
@@ -343,7 +343,7 @@ macro(z3_add_gparams_register_modules_rule)
   unset(_component_register_module_header_files)
 
   add_custom_command(OUTPUT "gparams_register_modules.cpp"
-    COMMAND "${PYTHON_EXECUTABLE}"
+    COMMAND "${Z3_PYTHON_EXECUTABLE}"
     "${PROJECT_SOURCE_DIR}/scripts/mk_gparams_register_modules_cpp.py"
     "${CMAKE_CURRENT_BINARY_DIR}"
     ${_register_module_header_files}

contrib/ci/Dockerfiles/z3_build.Dockerfile
@@ -13,7 +13,7 @@ ARG DOTNET_BINDINGS
 ARG JAVA_BINDINGS
 ARG NO_SUPPRESS_OUTPUT
 ARG PYTHON_BINDINGS
-ARG PYTHON_EXECUTABLE=/usr/bin/python2.7
+ARG Z3_PYTHON_EXECUTABLE=/usr/bin/python2.7
 ARG RUN_API_EXAMPLES
 ARG RUN_SYSTEM_TESTS
 ARG RUN_UNIT_TESTS
@@ -42,7 +42,7 @@ ENV \
   JAVA_BINDINGS=${JAVA_BINDINGS} \
   NO_SUPPRESS_OUTPUT=${NO_SUPPRESS_OUTPUT} \
   PYTHON_BINDINGS=${PYTHON_BINDINGS} \
-  PYTHON_EXECUTABLE=${PYTHON_EXECUTABLE} \
+  Z3_PYTHON_EXECUTABLE=${Z3_PYTHON_EXECUTABLE} \
   SANITIZER_PRINT_SUPPRESSIONS=${SANITIZER_PRINT_SUPPRESSIONS} \
   RUN_API_EXAMPLES=${RUN_API_EXAMPLES} \
   RUN_SYSTEM_TESTS=${RUN_SYSTEM_TESTS} \

contrib/ci/scripts/build_z3_cmake.sh
@@ -14,7 +14,7 @@ set -o pipefail
 : ${Z3_STATIC_BUILD?"Z3_STATIC_BUILD must be specified"}
 : ${Z3_USE_LIBGMP?"Z3_USE_LIBGMP must be specified"}
 : ${BUILD_DOCS?"BUILD_DOCS must be specified"}
-: ${PYTHON_EXECUTABLE?"PYTHON_EXECUTABLE must be specified"}
+: ${Z3_PYTHON_EXECUTABLE?"Z3_PYTHON_EXECUTABLE must be specified"}
 : ${PYTHON_BINDINGS?"PYTHON_BINDINGS must be specified"}
 : ${DOTNET_BINDINGS?"DOTNET_BINDINGS must be specified"}
 : ${JAVA_BINDINGS?"JAVA_BINDINGS must be specified"}
@@ -122,7 +122,7 @@ cd "${Z3_BUILD_DIR}"
 cmake \
   -G "${Z3_CMAKE_GENERATOR}" \
   -DCMAKE_BUILD_TYPE=${Z3_BUILD_TYPE} \
-  -DPYTHON_EXECUTABLE=${PYTHON_EXECUTABLE} \
+  -DZ3_PYTHON_EXECUTABLE=${Z3_PYTHON_EXECUTABLE} \
   -DCMAKE_INSTALL_PREFIX=${Z3_INSTALL_PREFIX} \
   -DWARNINGS_AS_ERRORS=${Z3_WARNINGS_AS_ERRORS} \
   "${ADDITIONAL_Z3_OPTS[@]}" \

contrib/ci/scripts/ci_defaults.sh
@@ -52,7 +52,7 @@ unset PLATFORM
 
 # NOTE: The following variables are not set here because
 # they are specific to the CI implementation
-# PYTHON_EXECUTABLE
+# Z3_PYTHON_EXECUTABLE
 # ASAN_DSO
 # Z3_SRC_DIR
 # Z3_BUILD_DIR

contrib/ci/scripts/travis_ci_linux_entry_point.sh
@@ -38,8 +38,8 @@ if [ -n "${BUILD_DOCS}" ]; then
   BUILD_OPTS+=("--build-arg" "BUILD_DOCS=${BUILD_DOCS}")
 fi
 
-if [ -n "${PYTHON_EXECUTABLE}" ]; then
-  BUILD_OPTS+=("--build-arg" "PYTHON_EXECUTABLE=${PYTHON_EXECUTABLE}")
+if [ -n "${Z3_PYTHON_EXECUTABLE}" ]; then
+  BUILD_OPTS+=("--build-arg" "Z3_PYTHON_EXECUTABLE=${Z3_PYTHON_EXECUTABLE}")
 fi
 
 if [ -n "${PYTHON_BINDINGS}" ]; then

contrib/ci/scripts/travis_ci_osx_entry_point.sh
@@ -21,7 +21,7 @@ fi
 
 # These variables are specific to the macOS TravisCI
 # implementation and are not set in `ci_defaults.sh`.
-export PYTHON_EXECUTABLE="${PYTHON_EXECUTABLE:-$(which python)}"
+export Z3_PYTHON_EXECUTABLE="${Z3_PYTHON_EXECUTABLE:-$(which python)}"
 export Z3_SRC_DIR="${TRAVIS_BUILD_DIR}"
 export Z3_BUILD_DIR="${Z3_SRC_DIR}/build"
 export Z3_SYSTEM_TEST_DIR="${Z3_SRC_DIR}/z3_system_test"

doc/CMakeLists.txt
@@ -58,7 +58,7 @@ endif()
 
 add_custom_target(api_docs ${ALWAYS_BUILD_DOCS_ARG}
   COMMAND
-  "${PYTHON_EXECUTABLE}" "${MK_API_DOC_SCRIPT}"
+  "${Z3_PYTHON_EXECUTABLE}" "${MK_API_DOC_SCRIPT}"
   --build "${PROJECT_BINARY_DIR}"
   --doxygen-executable "${DOXYGEN_EXECUTABLE}"
   --output-dir "${DOC_DEST_DIR}"

src/CMakeLists.txt
@@ -186,7 +186,7 @@ if (MSVC)
   set(dll_module_exports_file "${CMAKE_CURRENT_BINARY_DIR}/api_dll.def")
   add_custom_command(OUTPUT "${dll_module_exports_file}"
     COMMAND
-      "${PYTHON_EXECUTABLE}"
+      "${Z3_PYTHON_EXECUTABLE}"
       "${PROJECT_SOURCE_DIR}/scripts/mk_def_file.py"
       "${dll_module_exports_file}"
       "libz3"

src/api/CMakeLists.txt
@@ -18,7 +18,7 @@ foreach (gen_file ${generated_files})
 endforeach()
 
 add_custom_command(OUTPUT ${generated_files}
-  COMMAND "${PYTHON_EXECUTABLE}"
+  COMMAND "${Z3_PYTHON_EXECUTABLE}"
   "${PROJECT_SOURCE_DIR}/scripts/update_api.py"
   ${Z3_FULL_PATH_API_HEADER_FILES_TO_SCAN}
   "--api_output_dir"

src/api/dotnet/CMakeLists.txt
@@ -9,7 +9,7 @@ set(VER_TWEAK "${Z3_VERSION_TWEAK}")
 # Generate Native.cs
 set(Z3_DOTNET_NATIVE_FILE "${CMAKE_CURRENT_BINARY_DIR}/Native.cs")
 add_custom_command(OUTPUT "${Z3_DOTNET_NATIVE_FILE}"
-  COMMAND "${PYTHON_EXECUTABLE}"
+  COMMAND "${Z3_PYTHON_EXECUTABLE}"
     "${PROJECT_SOURCE_DIR}/scripts/update_api.py"
     ${Z3_FULL_PATH_API_HEADER_FILES_TO_SCAN}
     "--dotnet-output-dir"
@@ -27,7 +27,7 @@ add_custom_command(OUTPUT "${Z3_DOTNET_NATIVE_FILE}"
 # Generate Enumerations.cs
 set(Z3_DOTNET_CONST_FILE "${CMAKE_CURRENT_BINARY_DIR}/Enumerations.cs")
 add_custom_command(OUTPUT "${Z3_DOTNET_CONST_FILE}"
-  COMMAND "${PYTHON_EXECUTABLE}"
+  COMMAND "${Z3_PYTHON_EXECUTABLE}"
     "${PROJECT_SOURCE_DIR}/scripts/mk_consts_files.py"
     ${Z3_FULL_PATH_API_HEADER_FILES_TO_SCAN}
     "--dotnet-output-dir"

src/api/java/CMakeLists.txt
@@ -16,7 +16,7 @@ set(Z3_JAVA_PACKAGE_NAME "com.microsoft.z3")
 set(Z3_JAVA_NATIVE_JAVA "${CMAKE_CURRENT_BINARY_DIR}/Native.java")
 set(Z3_JAVA_NATIVE_CPP "${CMAKE_CURRENT_BINARY_DIR}/Native.cpp")
 add_custom_command(OUTPUT "${Z3_JAVA_NATIVE_JAVA}" "${Z3_JAVA_NATIVE_CPP}"
-  COMMAND "${PYTHON_EXECUTABLE}"
+  COMMAND "${Z3_PYTHON_EXECUTABLE}"
     "${PROJECT_SOURCE_DIR}/scripts/update_api.py"
     ${Z3_FULL_PATH_API_HEADER_FILES_TO_SCAN}
     "--java-output-dir"
@@ -74,7 +74,7 @@ foreach (enum_file ${Z3_JAVA_ENUMERATION_PACKAGE_FILES})
   )
 endforeach()
 add_custom_command(OUTPUT ${Z3_JAVA_ENUMERATION_PACKAGE_FILES_FULL_PATH}
-  COMMAND "${PYTHON_EXECUTABLE}"
+  COMMAND "${Z3_PYTHON_EXECUTABLE}"
     "${PROJECT_SOURCE_DIR}/scripts/mk_consts_files.py"
     ${Z3_FULL_PATH_API_HEADER_FILES_TO_SCAN}
     "--java-output-dir"

src/api/python/CMakeLists.txt
@@ -33,7 +33,7 @@ endforeach()
 
 # Generate z3core.py
 add_custom_command(OUTPUT "${z3py_bindings_build_dest}/z3/z3core.py"
-  COMMAND "${PYTHON_EXECUTABLE}"
+  COMMAND "${Z3_PYTHON_EXECUTABLE}"
     "${PROJECT_SOURCE_DIR}/scripts/update_api.py"
     ${Z3_FULL_PATH_API_HEADER_FILES_TO_SCAN}
     "--z3py-output-dir"
@@ -51,7 +51,7 @@ list(APPEND build_z3_python_bindings_target_depends "${z3py_bindings_build_dest}
 
 # Generate z3consts.py
 add_custom_command(OUTPUT "${z3py_bindings_build_dest}/z3/z3consts.py"
-  COMMAND "${PYTHON_EXECUTABLE}"
+  COMMAND "${Z3_PYTHON_EXECUTABLE}"
     "${PROJECT_SOURCE_DIR}/scripts/mk_consts_files.py"
     ${Z3_FULL_PATH_API_HEADER_FILES_TO_SCAN}
     "--z3py-output-dir"
@@ -98,7 +98,7 @@ if (Z3_INSTALL_PYTHON_BINDINGS)
   if (NOT DEFINED CMAKE_INSTALL_PYTHON_PKG_DIR)
     message(STATUS "CMAKE_INSTALL_PYTHON_PKG_DIR not set. Trying to guess")
     execute_process(
-      COMMAND "${PYTHON_EXECUTABLE}" "-c"
+      COMMAND "${Z3_PYTHON_EXECUTABLE}" "-c"
         "import distutils.sysconfig; print(distutils.sysconfig.get_python_lib())"
       RESULT_VARIABLE exit_code
       OUTPUT_VARIABLE CMAKE_INSTALL_PYTHON_PKG_DIR

src/ast/pattern/CMakeLists.txt
@@ -7,7 +7,7 @@ if (EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/database.h")
 endif()
 
 add_custom_command(OUTPUT "database.h"
-  COMMAND "${PYTHON_EXECUTABLE}"
+  COMMAND "${Z3_PYTHON_EXECUTABLE}"
           "${PROJECT_SOURCE_DIR}/scripts/mk_pat_db.py"
           "${CMAKE_CURRENT_SOURCE_DIR}/database.smt2"
           "${CMAKE_CURRENT_BINARY_DIR}/database.h"

contrib/ci/scripts/test_z3_examples_cmake.sh
@@ -11,7 +11,7 @@ set -o pipefail
 : ${Z3_SRC_DIR?"Z3_SRC_DIR must be specified"}
 : ${Z3_BUILD_DIR?"Z3_BUILD_DIR must be specified"}
 : ${PYTHON_BINDINGS?"PYTHON_BINDINGS must be specified"}
-: ${PYTHON_EXECUTABLE?"PYTHON_EXECUTABLE must be specified"}
+: ${Z3_PYTHON_EXECUTABLE?"Z3_PYTHON_EXECUTABLE must be specified"}
 : ${DOTNET_BINDINGS?"DOTNET_BINDINGS must be specified"}
 : ${JAVA_BINDINGS?"JAVA_BINDINGS must be specified"}
 : ${UBSAN_BUILD?"UBSAN_BUILD must be specified"}
@@ -74,17 +74,17 @@ if [ "X${PYTHON_BINDINGS}" = "X1" ]; then
     # Too slow when doing ASan Debug build
     echo "Skipping all_interval_series.py under ASan Debug build"
   else
-    run_quiet run_non_native_binding ${PYTHON_EXECUTABLE} python/all_interval_series.py
+    run_quiet run_non_native_binding ${Z3_PYTHON_EXECUTABLE} python/all_interval_series.py
   fi
-  run_quiet run_non_native_binding ${PYTHON_EXECUTABLE} python/complex.py
-  run_quiet run_non_native_binding ${PYTHON_EXECUTABLE} python/example.py
+  run_quiet run_non_native_binding ${Z3_PYTHON_EXECUTABLE} python/complex.py
+  run_quiet run_non_native_binding ${Z3_PYTHON_EXECUTABLE} python/example.py
   # FIXME: `hamiltonian.py` example is disabled because its too slow.
-  #${PYTHON_EXECUTABLE} python/hamiltonian.py
-  run_quiet run_non_native_binding ${PYTHON_EXECUTABLE} python/marco.py
-  run_quiet run_non_native_binding ${PYTHON_EXECUTABLE} python/mss.py
-  run_quiet run_non_native_binding ${PYTHON_EXECUTABLE} python/socrates.py
-  run_quiet run_non_native_binding ${PYTHON_EXECUTABLE} python/visitor.py
-  run_quiet run_non_native_binding ${PYTHON_EXECUTABLE} python/z3test.py
+  #${Z3_PYTHON_EXECUTABLE} python/hamiltonian.py
+  run_quiet run_non_native_binding ${Z3_PYTHON_EXECUTABLE} python/marco.py
+  run_quiet run_non_native_binding ${Z3_PYTHON_EXECUTABLE} python/mss.py
+  run_quiet run_non_native_binding ${Z3_PYTHON_EXECUTABLE} python/socrates.py
+  run_quiet run_non_native_binding ${Z3_PYTHON_EXECUTABLE} python/visitor.py
+  run_quiet run_non_native_binding ${Z3_PYTHON_EXECUTABLE} python/z3test.py
 fi
 
 if [ "X${DOTNET_BINDINGS}" = "X1" ]; then

contrib/ci/scripts/test_z3_system_tests.sh
@@ -8,7 +8,7 @@ set -o pipefail
 : ${Z3_BUILD_TYPE?"Z3_BUILD_TYPE must be specified"}
 : ${RUN_SYSTEM_TESTS?"RUN_SYSTEM_TESTS must be speicifed"}
 : ${PYTHON_BINDINGS?"PYTHON_BINDINGS must be specified"}
-: ${PYTHON_EXECUTABLE?"PYTHON_EXECUTABLE must be specified"}
+: ${Z3_PYTHON_EXECUTABLE?"Z3_PYTHON_EXECUTABLE must be specified"}
 : ${Z3_SYSTEM_TEST_DIR?"Z3_SYSTEM_TEST_DIR must be specified"}
 : ${UBSAN_BUILD?"UBSAN_BUILD must be specified"}
 
@@ -43,12 +43,12 @@ fi
 ###############################################################################
 
 # SMTLIBv2 tests
-${PYTHON_EXECUTABLE} scripts/test_benchmarks.py "${Z3_EXE}" regressions/smt2
+${Z3_PYTHON_EXECUTABLE} scripts/test_benchmarks.py "${Z3_EXE}" regressions/smt2
 
-${PYTHON_EXECUTABLE} scripts/test_benchmarks.py "${Z3_EXE}" regressions/smt2-extra
+${Z3_PYTHON_EXECUTABLE} scripts/test_benchmarks.py "${Z3_EXE}" regressions/smt2-extra
 
 if [ "X${Z3_BUILD_TYPE}" = "XDebug" ]; then
-  ${PYTHON_EXECUTABLE} scripts/test_benchmarks.py "${Z3_EXE}" regressions/smt2-debug
+  ${Z3_PYTHON_EXECUTABLE} scripts/test_benchmarks.py "${Z3_EXE}" regressions/smt2-debug
 fi
 
 if [ "X${PYTHON_BINDINGS}" = "X1" ]; then
@@ -63,7 +63,7 @@ if [ "X${PYTHON_BINDINGS}" = "X1" ]; then
     # to work.
     echo "FIXME: Skipping python binding tests when building with ASan"
   else
-    run_non_native_binding ${PYTHON_EXECUTABLE} scripts/test_pyscripts.py "${Z3_LIB_DIR}" regressions/python/
+    run_non_native_binding ${Z3_PYTHON_EXECUTABLE} scripts/test_pyscripts.py "${Z3_LIB_DIR}" regressions/python/
   fi
 fi
 