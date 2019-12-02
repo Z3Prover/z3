ad309e53a12d41c2b7a2dd50093052619a02fb60
Nikolaj Bjorner <nbjorner@microsoft.com>
2019-12-01 16:06:14 -0800

    fix #2766

--- src/smt/theory_dense_diff_logic_def.h
+++ src/smt/theory_dense_diff_logic_def.h
@@ -150,13 +150,13 @@ namespace smt {
         numeral offset(_k);
         app * s, * t;
         expr *arg1, *arg2;
-        if (m_autil.is_add(lhs, arg1, arg2) && is_times_minus_one(arg2, s)) {
+        if (m_autil.is_add(lhs, arg1, arg2) && is_times_minus_one(arg2, s) && !m_autil.is_arith_expr(s) && !m_autil.is_arith_expr(arg1)) {
             t = to_app(arg1);
         }
-        else if (m_autil.is_add(lhs, arg1, arg2) && is_times_minus_one(arg1, s)) {
+        else if (m_autil.is_add(lhs, arg1, arg2) && is_times_minus_one(arg1, s) && !m_autil.is_arith_expr(s) && !m_autil.is_arith_expr(arg2)) {
             t = to_app(arg2);
         }
-        else if (m_autil.is_mul(lhs, arg1, arg2) && m_autil.is_minus_one(arg1)) {
+        else if (m_autil.is_mul(lhs, arg1, arg2) && m_autil.is_minus_one(arg1) && !m_autil.is_arith_expr(arg2)) {
             s = to_app(arg2);
             t = mk_zero_for(s);
         }
