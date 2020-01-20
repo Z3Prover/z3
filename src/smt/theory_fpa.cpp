321bad2c84c9bb0c58f44d5797bdf993b47b09d4
Christoph M. Wintersteiger <cwinter@microsoft.com>
2020-01-20 17:06:35 +0000

    Fix for implicit consts in FPA models. Fixes #2865.

--- src/smt/theory_fpa.cpp
+++ src/smt/theory_fpa.cpp
@@ -724,8 +724,15 @@ namespace smt {
     void theory_fpa::init_model(model_generator & mg) {
         TRACE("t_fpa", tout << "initializing model" << std::endl; display(tout););
         ast_manager & m = get_manager();
+        context & ctx = get_context();
         m_factory = alloc(fpa_value_factory, m, get_family_id());
         mg.register_factory(m_factory);
+
+        for (enode* n : ctx.enodes()) {
+            theory_var v = n->get_th_var(get_family_id());
+            if (v != -1 && is_uninterp_const(n->get_owner()))
+                ctx.mark_as_relevant(n->get_owner());
+        }
     }
 
     enode* theory_fpa::ensure_enode(expr* e) {
