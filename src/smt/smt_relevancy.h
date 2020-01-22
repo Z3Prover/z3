55f62fcaed255e47f7557e4f5dd6ecd04abd8e88
Nikolaj Bjorner <nbjorner@microsoft.com>
2020-01-22 16:16:44 -0600

    fix #2865
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/smt/smt_relevancy.h
+++ src/smt/smt_relevancy.h
@@ -95,6 +95,7 @@ namespace smt {
 
         context & get_context() { return m_context; }
 
+
         /**
            \brief Install an event handler that is invoked whenever n is marked as relevant.
         */
