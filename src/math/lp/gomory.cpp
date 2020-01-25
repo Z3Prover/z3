c26725c91c59eff766b9fcb35f3a460267b07b3b
Lev Nachmanson <levnach@hotmail.com>
2020-01-24 16:55:56 -0800

    rebase with Z3Prover/z3/master
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- src/math/lp/gomory.cpp
+++ src/math/lp/gomory.cpp
@@ -268,13 +268,7 @@ public:
     }
 
     lia_move create_cut() {
-        TRACE("gomory_cut",
-              print_linear_combination_of_column_indices_only(m_row, tout << "applying cut at:\n"); tout << std::endl;
-              for (auto & p : m_row) {
-                  m_int_solver.m_lar_solver->m_mpq_lar_core_solver.m_r_solver.print_column_info(p.var(), tout);
-              }
-              tout << "inf_col = " << m_inf_col << std::endl;
-              );
+        TRACE("gomory_cut", dump(tout););
         
         // gomory will be   t <= k and the current solution has a property t > k
         m_k = 1;
