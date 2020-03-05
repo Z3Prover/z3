8b0d540ccae19dc7154b9386124af1989bed77c0
Nikolaj Bjorner <nbjorner@microsoft.com>
2020-03-05 10:35:24 +0100

    fix #3148
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/ast/rewriter/bv_rewriter.cpp
+++ src/ast/rewriter/bv_rewriter.cpp
@@ -273,13 +273,13 @@ bool bv_rewriter::are_eq_upto_num(expr * _a, expr * _b,
         }
     }
     if (!aadd && badd) {
-        if (to_app(_a)->get_num_args() != 2 || !has_num_a || to_app(_a)->get_arg(0) != _b)
+        if (!is_app(_a) || to_app(_a)->get_num_args() != 2 || !has_num_a || to_app(_a)->get_arg(0) != _b)
             return false;
         common = _b;
         return true;
     }
     if (aadd && !badd) {
-        if (to_app(_b)->get_num_args() != 2 || !has_num_b || to_app(_b)->get_arg(0) != _a)
+        if (!is_app(_b) || to_app(_b)->get_num_args() != 2 || !has_num_b || to_app(_b)->get_arg(0) != _a)
             return false;
         common = _a;
         return true;
