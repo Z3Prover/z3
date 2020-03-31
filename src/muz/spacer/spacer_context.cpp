2ec00cb81ddf2257147d7ed66f5170e2cd000164
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-03-31 11:21:05 -0400

    Replace is_null with is_non_empty_string in spacer params

--- src/muz/spacer/spacer_context.cpp
+++ src/muz/spacer/spacer_context.cpp
@@ -3339,7 +3339,7 @@ bool context::is_reachable(pob &n)
 
 void context::dump_json()
 {
-    if (!m_params.spacer_print_json().is_null()) {
+    if (m_params.spacer_print_json().is_non_empty_string()) {
         std::ofstream of;
         of.open(m_params.spacer_print_json().bare_str());
         m_json_marshaller.marshal(of);
@@ -4003,7 +4003,7 @@ void context::add_constraint (expr *c, unsigned level)
 }
 
 void context::new_lemma_eh(pred_transformer &pt, lemma *lem) {
-    if (!m_params.spacer_print_json().is_null())
+    if (m_params.spacer_print_json().is_non_empty_string())
         m_json_marshaller.register_lemma(lem);
     bool handle=false;
     for (unsigned i = 0; i < m_callbacks.size(); i++) {
@@ -4027,7 +4027,7 @@ void context::new_lemma_eh(pred_transformer &pt, lemma *lem) {
 }
 
 void context::new_pob_eh(pob *p) {
-    if (!m_params.spacer_print_json().is_null())
+    if (m_params.spacer_print_json().is_non_empty_string())
         m_json_marshaller.register_pob(p);
 }
 
