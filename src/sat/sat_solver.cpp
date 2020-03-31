55c285c0df6aaa31ed6ce54ca3f805bb7788aae9
Nikolaj Bjorner <nbjorner@microsoft.com>
2020-03-31 14:01:00 -0700

    fix #3620
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/sat/sat_solver.cpp
+++ src/sat/sat_solver.cpp
@@ -1276,6 +1276,8 @@ namespace sat {
     };
 
     lbool solver::invoke_local_search(unsigned num_lits, literal const* lits) {
+        literal_vector _lits(num_lits, lits);
+        for (literal lit : m_user_scope_literals) _lits.push_back(~lit);
         SASSERT(m_local_search);
         scoped_ptr<i_local_search> _ptr = m_local_search;
         if (inconsistent()) return l_false;
@@ -1284,7 +1286,7 @@ namespace sat {
         m_local_search->add(*this);
         m_local_search->updt_params(m_params);
         scoped_rl.push_child(&(m_local_search->rlimit()));
-        lbool r = m_local_search->check(num_lits, lits, nullptr);
+        lbool r = m_local_search->check(_lits.size(), _lits.c_ptr(), nullptr);
         if (r == l_true) {
             m_model = m_local_search->get_model();
             m_model_is_current = true;
