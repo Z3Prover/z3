c2a59a89af83433faa0b6bfe1cdb3bb0ffdcbf96
Nikolaj Bjorner <nbjorner@microsoft.com>
2020-05-18 15:32:23 -0700

    thanks to https://github.com/Z3Prover/z3/pull/4382#issuecomment-630468832_
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/util/mpz.h
+++ src/util/mpz.h
@@ -20,9 +20,7 @@ Revision History:
 #define MPZ_H_
 
 #include<string>
-#ifndef SINGLE_THREAD
-#include<mutex>
-#endif
+#include "util/mutex.h"
 #include "util/util.h"
 #include "util/small_object_allocator.h"
 #include "util/trace.h"
