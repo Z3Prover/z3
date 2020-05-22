131dfc21011598eddafe3294c7772377fb77b2ca
Nikolaj Bjorner <nbjorner@microsoft.com>
2020-05-21 21:04:16 -0700

    na

--- src/smt/smt_context.cpp
+++ src/smt/smt_context.cpp
@@ -1601,7 +1601,7 @@ namespace smt {
             if (inconsistent())
                 return false;
         }
-#if 1
+#if 0
         if (at_search_level() && induction::should_try(*this)) {
             get_induction()();
         }
