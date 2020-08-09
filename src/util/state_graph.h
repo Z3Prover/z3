11f28f3837c39a98d2bb0d1e9039b4982063d746
Margus Veanes <margus@microsoft.com>
2020-08-09 15:04:14 -0700

    dot output support for state_graph and moved dgml and dot output under CASSERT

--- src/util/state_graph.h
+++ src/util/state_graph.h
@@ -112,7 +112,14 @@ private:
     bool is_subset(state_set set1, state_set set2) const;
     bool is_disjoint(state_set set1, state_set set2) const;
     bool check_invariant() const;
-    void write_dgml();
+    /*
+    Output the whole state graph in dgml format into the file '.z3-state-graph.dgml'
+    */
+    bool write_dgml();
+    /*
+    Output the whole state graph in dot format into the file '.z3-state-graph.dot'
+    */
+    bool write_dot();
     #endif
 
     /*
