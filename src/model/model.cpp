35900ee8eaf890edba32ccc877830073dd9b914f
Nikolaj Bjorner <nbjorner@microsoft.com>
2020-11-27 14:41:28 -0800

    avoid crash from #4772
    To take care of "When I use options fp.xform.slice=false fp.xform.inline_eager=false Z3 actually seg-faults."

--- src/model/model.cpp
+++ src/model/model.cpp
@@ -451,8 +451,10 @@ expr_ref model::cleanup_expr(top_sort& ts, expr* e, unsigned current_partition) 
                 // only expand auxiliary definitions that occur once.
                 if (can_inline_def(ts, f)) {
                     fi = get_func_interp(f);
-                    new_t = fi->get_array_interp(f);
-                    TRACE("model", tout << "array interpretation:" << new_t << "\n";);
+                    if (fi) {
+                        new_t = fi->get_array_interp(f);
+                        TRACE("model", tout << "array interpretation:" << new_t << "\n";);
+                    }
                 }
             }
 
