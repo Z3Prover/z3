e1572096ca707c58f7cb91337dfba71d4d9f4592
Nuno Lopes <nlopes@microsoft.com>
2021-02-07 12:14:52 +0000

    delete some dead code

--- src/api/api_stats.cpp
+++ src/api/api_stats.cpp
@@ -31,7 +31,6 @@ extern "C" {
         to_stats_ref(s).display_smt2(buffer);
         std::string result = buffer.str();
         // Hack for removing the trailing '\n'
-        result = buffer.str();
         SASSERT(result.size() > 0);
         result.resize(result.size()-1);
         return mk_c(c)->mk_external_string(std::move(result));
