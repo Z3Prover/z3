8ca2de41db79a266b58124c4849fd61d4a8f6c1e
Nikolaj Bjorner <nbjorner@microsoft.com>
2021-02-09 09:59:21 -0800

    turn on from/to code handling #5007 samples

--- src/smt/theory_seq.cpp
+++ src/smt/theory_seq.cpp
@@ -3099,8 +3099,8 @@ void theory_seq::relevant_eh(app* n) {
     if (m_util.str.is_replace_all(n) ||
         m_util.str.is_replace_re(n) ||
         m_util.str.is_replace_re_all(n) ||
-        m_util.str.is_from_code(n) ||
-        m_util.str.is_to_code(n) ||
+        // m_util.str.is_from_code(n) ||
+        // m_util.str.is_to_code(n) ||
         m_util.str.is_is_digit(n)) {
         add_unhandled_expr(n);
     }
