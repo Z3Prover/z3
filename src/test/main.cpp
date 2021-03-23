b918f121ef93b61346583be7a1b896fca1bbdd0f
Luca Bruno <lucab@debian.org>
2021-03-23 18:25:59 +0000

    zstring: fix encode rountrip for '\' as printable ASCII (#5120)
    This fixes encode roundtripping for all printable ASCII characters.
    In particular, this now leaves a plain '\' untouched by the
    encoding logic, instead of converting it to escaped hex-digits.
    It also adds unit testing covering this specific zstring encoding
    property, in order to avoid future regressions.

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -215,6 +215,7 @@ int main(int argc, char ** argv) {
     TST(prime_generator);
     TST(permutation);
     TST(nlsat);
+    TST(zstring);
     if (test_all) return 0;
     TST(ext_numeral);
     TST(interval);
