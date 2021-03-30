b8493e9a57ba28dce09be77b66098cb78fc55b3c
Nikolaj Bjorner <nbjorner@microsoft.com>
2021-03-30 09:27:03 -0700

    add unit test stub
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

---
+++ src/test/polysat.cpp
@@ -0,0 +1,15 @@
+#include "math/polysat/polysat.h"
+
+namespace polysat {
+    // test resolve, factoring routines
+    // auxiliary 
+    static void test1() {
+
+    }
+}
+
+// also add test that loads from a file and runs the polysat engine.
+
+void tst_polysat() {
+    polysat::test1();
+}
