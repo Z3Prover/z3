878847179fa20ebfcaf7a87bc89248c212b662bd
Nikolaj Bjorner <nbjorner@microsoft.com>
2021-04-10 15:30:17 -0700

    fix #5144

--- src/smt/seq_ne_solver.cpp
+++ src/smt/seq_ne_solver.cpp
@@ -144,7 +144,7 @@ bool theory_seq::propagate_ne2eq(unsigned idx, expr_ref_vector const& es) {
     expr_ref e(m), head(m), tail(m);
     e = mk_concat(es, es[0]->get_sort());
     m_sk.decompose(e, head, tail);
-    propagate_eq(n.dep(), n.lits(), e, mk_concat(head, tail), false);
+    propagate_eq(n.dep(), n.lits(), e, mk_concat(head, tail), true);
     return true;
 }
  
