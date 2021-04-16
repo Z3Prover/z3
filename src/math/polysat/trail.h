12ccd99e84048885e7669f4f62858af63b85644b
Nikolaj Bjorner <nbjorner@microsoft.com>
2021-04-15 17:39:00 -0700

    trail
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

---
+++ src/math/polysat/trail.h
@@ -0,0 +1,30 @@
+/*++
+Copyright (c) 2021 Microsoft Corporation
+
+Module Name:
+
+    polysat trail
+
+Author:
+
+    Nikolaj Bjorner (nbjorner) 2021-03-19
+    Jakob Rath 2021-04-6
+
+--*/
+#pragma once
+#include "math/polysat/types.h"
+
+namespace polysat {
+
+    enum trail_instr_t { 
+        qhead_i,
+        add_var_i,
+        inc_level_i,
+        viable_i,
+        assign_i,
+        just_i
+    };
+
+    
+
+}
