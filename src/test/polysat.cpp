e6da3f2795a2d850b9842edd12c41aa3f1ebeda6
alex <alex@certora.com>
2021-04-27 11:47:22 +0200

    whitespace...

--- src/test/polysat.cpp
+++ src/test/polysat.cpp
@@ -174,7 +174,7 @@ namespace polysat {
     static void test_monot() {
         scoped_solver s;
         auto bw = 5;
-        
+
         auto tb1 = s.var(s.add_var(bw));
         auto tb2 = s.var(s.add_var(bw));
         auto a = s.var(s.add_var(bw));
@@ -184,39 +184,39 @@ namespace polysat {
         auto elastic1 = s.var(s.add_var(bw));
         auto elastic2 = s.var(s.add_var(bw));
         auto err = s.var(s.add_var(bw));
-        
+
         auto rem1 = s.var(s.add_var(bw));
         auto quot2 = s.var(s.add_var(bw));
         auto rem2 = s.var(s.add_var(bw));
         auto rem3 = s.var(s.add_var(bw));
         auto quot4 = s.var(s.add_var(bw));
         auto rem4 = s.var(s.add_var(bw));
-        
+
         s.add_diseq(elastic1);
-        
+
         // tb1 = (v * base1) / elastic1;
         s.add_eq((tb1 * elastic1) + rem1 - (v * base1));
-        
+
         // quot2 = (a * base1) / elastic1
         s.add_eq((quot2 * elastic1) + rem2 - (a * base1));
-        
+
         s.add_eq(base1 + quot2 - base2);
-        
+
         s.add_eq(elastic1 + a - elastic2);
-        
+
         // tb2 = ((v * base2) / elastic2);
         s.add_eq((tb2 * elastic2) + rem3 - (v * base2));
-        
+
         // quot4 = v / (elastic1 + a);
         s.add_eq((quot4 * (elastic1 + a)) + rem4 - v);
-        
+
         s.add_eq(quot4 + 1 - err);
-        
+
         push()
         s.add_ult(tb1, tb2);
         s.check();
         pop()
-        
+
         push()
         s.add_ult(tb1 + err, tb2);
         s.check();
