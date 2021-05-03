db5515d73b146442012d4aafb8f8e167f0c6e64d
Jakob Rath <git@jakobrath.eu>
2021-05-03 14:03:59 +0200

    Rename to neg_cond

--- src/math/polysat/forbidden_intervals.cpp
+++ src/math/polysat/forbidden_intervals.cpp
@@ -20,7 +20,7 @@ namespace polysat {
 
     struct fi_record {
         eval_interval interval;
-        scoped_ptr<constraint> cond;  // could be multiple constraints later
+        scoped_ptr<constraint> neg_cond;  // could be multiple constraints later
         constraint* src;
     };
 
@@ -72,15 +72,15 @@ namespace polysat {
         for (constraint* c : conflict) {
             LOG("constraint: " << *c);
             eval_interval interval = eval_interval::full();
-            constraint* cond = nullptr;  // TODO: change to scoped_ptr
-            if (c->forbidden_interval(s, v, interval, cond)) {
+            constraint* neg_cond = nullptr;  // TODO: change to scoped_ptr
+            if (c->forbidden_interval(s, v, interval, neg_cond)) {
                 LOG("~> interval: " << interval);
-                if (cond)
-                    LOG("       cond: " << *cond);
+                if (neg_cond)
+                    LOG("       neg_cond: " << *neg_cond);
                 else
-                    LOG("       cond: <null>");
+                    LOG("       neg_cond: <null>");
                 if (interval.is_currently_empty()) {
-                    dealloc(cond);
+                    dealloc(neg_cond);
                     continue;
                 }
                 if (interval.is_full())
@@ -92,7 +92,7 @@ namespace polysat {
                         longest_i = records.size();
                     }
                 }
-                records.push_back({std::move(interval), cond, c});
+                records.push_back({std::move(interval), neg_cond, c});
                 if (has_full)
                     break;
             }
@@ -153,9 +153,9 @@ namespace polysat {
             literals.push_back(c);
             // Side conditions
             // TODO: check whether the condition is subsumed by c?  maybe at the end do a "lemma reduction" step, to try and reduce branching?
-            scoped_ptr<constraint>& cond = records[i].cond;
-            if (cond)
-                literals.push_back(cond.detach());
+            scoped_ptr<constraint>& neg_cond = records[i].neg_cond;
+            if (neg_cond)
+                literals.push_back(neg_cond.detach());
         }
 
         out_lemma = std::move(literals);
