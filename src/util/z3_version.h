7b57c2345bd7ef51b9e2857593d04ec687a616c2
gram <mail@orsinium.dev>
2021-05-23 11:59:07 +0200

    drop accidentally commited src/util/z3_version.h

--- src/util/z3_version.h
+++
@@ -1,7 +0,0 @@
-// automatically generated file.
-#define Z3_MAJOR_VERSION   4
-#define Z3_MINOR_VERSION   8
-#define Z3_BUILD_NUMBER    11
-#define Z3_REVISION_NUMBER 0
-
-#define Z3_FULL_VERSION    "Z3 4.8.11.0"
