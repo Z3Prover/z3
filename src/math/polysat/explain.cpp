0aa2118522a0464d4c87b38de468657013a509de
Jakob Rath <git@jakobrath.eu>
2021-06-16 11:06:14 +0200

    Add stub for conflict explainer

---
+++ src/math/polysat/explain.cpp
@@ -0,0 +1,19 @@
+/*++
+Copyright (c) 2021 Microsoft Corporation
+
+Module Name:
+
+    Conflict explanation / resolution
+
+Author:
+
+    Nikolaj Bjorner (nbjorner) 2021-03-19
+    Jakob Rath 2021-04-6
+
+--*/
+#include "math/polysat/explain.h"
+#include "math/polysat/log.h"
+
+namespace polysat {
+
+}
---
+++ src/math/polysat/explain.h
@@ -0,0 +1,23 @@
+/*++
+Copyright (c) 2021 Microsoft Corporation
+
+Module Name:
+
+    Conflict explanation / resolution
+
+Author:
+
+    Nikolaj Bjorner (nbjorner) 2021-03-19
+    Jakob Rath 2021-04-6
+
+--*/
+#pragma once
+#include "math/polysat/constraint.h"
+#include "math/polysat/interval.h"
+#include "math/polysat/solver.h"
+
+namespace polysat {
+    class conflict_explainer {
+        // TODO
+    };
+}
