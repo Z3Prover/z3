1ed1e416b7cda7afdd05697ccf4e19eec8c8bdc2
Nikolaj Bjorner <nbjorner@microsoft.com>
2021-07-02 16:34:33 -0700

    na
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/math/bigfix/u256.h
+++ src/math/bigfix/u256.h
@@ -6,6 +6,10 @@ class u256 {
     uint64_t m_num[4];
 public:
     u256() { memset(this, 0, sizeof(*this)); }
+    u256(uint64_t n);
     u256(uint64_t const* v) { memcpy(m_num, v, sizeof(*this)); }
     u256 operator*(u256 const& other) const;
+    u256 operator+(u256 const& other) const;
+    u256 operator-(u256 const& other) const;
+    
 };
