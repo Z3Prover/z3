8f8476990cf8b54957753c075d9679d4d55e751d
Henrich Lauko <henrich.lauko@trailofbits.com>
2021-10-28 15:33:53 +0200

    rewriter: fix logical-op-parentheses warnings

--- src/ast/rewriter/seq_rewriter.cpp
+++ src/ast/rewriter/seq_rewriter.cpp
@@ -3181,7 +3181,7 @@ void seq_rewriter::mk_antimirov_deriv_rec(expr* e, expr* r, expr* path, expr_ref
     else if (re().is_loop(r, r1, lo))
         result = mk_antimirov_deriv_concat(mk_antimirov_deriv(e, r1, path), re().mk_loop(r1, lo - 1));
     else if (re().is_loop(r, r1, lo, hi)) {
-        if (lo == 0 && hi == 0 || hi < lo)
+        if ((lo == 0 && hi == 0) || hi < lo)
             result = nothing();
         else
             result = mk_antimirov_deriv_concat(mk_antimirov_deriv(e, r1, path), re().mk_loop(r1, (lo == 0 ? 0 : lo - 1), hi - 1));
@@ -3403,7 +3403,7 @@ expr_ref  seq_rewriter::simplify_path(expr* path) {
             result = simplify_path(t);
         else if (m().is_true(t))
             result = simplify_path(h);
-        else if (m().is_eq(h, lhs, rhs) || m().is_not(h, h1) && m().is_eq(h1, lhs, rhs))
+        else if (m().is_eq(h, lhs, rhs) || (m().is_not(h, h1) && m().is_eq(h1, lhs, rhs)))
             elim_condition(lhs, result);
     }
     return result;
