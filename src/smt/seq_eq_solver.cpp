dd1e0fc561196bbe097e45fe5876a00ca2d152c3
Nikolaj Bjorner <nbjorner@microsoft.com>
2021-11-03 08:49:33 -0700

    #5643

--- src/smt/seq_eq_solver.cpp
+++ src/smt/seq_eq_solver.cpp
@@ -1145,7 +1145,7 @@ bool theory_seq::solve_nth_eq(expr_ref_vector const& ls, expr_ref_vector const& 
         m.inc_ref(rhs);
         m.inc_ref(ls[0]);
         m_nth_eq2_cache.insert(std::make_pair(rhs, ls[0]));
-        ctx.push_trail(remove_obj_pair_map(m, m_nth_eq2_cache, rhs, ls[0]));
+        get_trail_stack().push(remove_obj_pair_map(m, m_nth_eq2_cache, rhs, ls[0]));
         ls1.push_back(s);        
         if (!idx_is_zero) rs1.push_back(m_sk.mk_pre(s, idx)); 
         rs1.push_back(m_util.str.mk_unit(rhs)); 
