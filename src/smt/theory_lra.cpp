b6f7deacf4dbdefc3672524b80ae1af7fa1e0507
Nikolaj Bjorner <nbjorner@microsoft.com>
2021-11-12 11:36:42 -0800

    fix #5663

--- src/smt/theory_lra.cpp
+++ src/smt/theory_lra.cpp
@@ -3477,7 +3477,10 @@ public:
                 st = lp::lp_status::FEASIBLE;
                 lp().restore_x();
             }
-                
+            if (m_nla && st == lp::lp_status::OPTIMAL) {
+                st = lp::lp_status::FEASIBLE;
+                lp().restore_x();
+            }                
         }
         switch (st) {
         case lp::lp_status::OPTIMAL: {
