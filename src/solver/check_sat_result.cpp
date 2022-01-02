0ca5e7207ea3433f7fc5dd76da0604396a55d97a
Nikolaj Bjorner <nbjorner@microsoft.com>
2022-01-02 11:35:55 -0800

    #5746

--- src/solver/check_sat_result.cpp
+++ src/solver/check_sat_result.cpp
@@ -21,7 +21,8 @@ Notes:
 void check_sat_result::set_reason_unknown(event_handler& eh) {
     switch (eh.caller_id()) {
     case UNSET_EH_CALLER: 
-        set_reason_unknown("unclassifed exception");
+        if (reason_unknown() == "")
+            set_reason_unknown("unclassifed exception");
         break;
     case CTRL_C_EH_CALLER:
         set_reason_unknown("interrupted from keyboard");
