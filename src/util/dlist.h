e8e64d3098b3c22a0d616c512ddc3bef5bc27302
Jakob Rath <git@jakobrath.eu>
2022-08-01 14:53:41 +0200

    dlist::insert_before/after

--- src/util/dlist.h
+++ src/util/dlist.h
@@ -40,6 +40,22 @@ public:
         remove_from(list, head);
         return head;
     }
+
+    void insert_after(T* elem) {
+        T* next = this->m_next;
+        elem->m_prev = next->m_prev;
+        elem->m_next = next;
+        this->m_next = elem;
+        next->m_prev = elem;
+    }
+
+    void insert_before(T* elem) {
+        T* prev = this->m_prev;
+        elem->m_next = prev->m_next;
+        elem->m_prev = prev;
+        prev->m_next = elem;
+        this->m_prev = elem;
+    }
     
     static void remove_from(T*& list, T* elem) {
         if (list->m_next == list) {
