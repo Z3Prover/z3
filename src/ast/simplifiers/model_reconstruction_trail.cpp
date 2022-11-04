626380b3c7573da941bed303e4548cac49b17953
Nikolaj Bjorner <nbjorner@microsoft.com>
2022-11-03 22:08:21 -0700

    fixing build
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/ast/simplifiers/model_reconstruction_trail.cpp
+++ src/ast/simplifiers/model_reconstruction_trail.cpp
@@ -12,6 +12,7 @@ Author:
 --*/
 
 
+#include "ast/for_each_expr.h"
 #include "ast/simplifiers/model_reconstruction_trail.h"
 #include "ast/converters/generic_model_converter.h"
 
@@ -20,9 +21,9 @@ void model_reconstruction_trail::replay(dependent_expr const& d, vector<dependen
     // accumulate a set of dependent exprs, updating m_trail to exclude loose 
     // substitutions that use variables from the dependent expressions.
     ast_mark free_vars;
-    auto [f, dep] = d;
+    auto [f, dep] = d();
     for (expr* t : subterms::all(expr_ref(f, m))) 
-        free_vars.mark(t);
+        free_vars.mark(t, true);
     
     NOT_IMPLEMENTED_YET();
 }
@@ -32,7 +33,7 @@ void model_reconstruction_trail::replay(dependent_expr const& d, vector<dependen
  */
 model_converter_ref model_reconstruction_trail::get_model_converter() {
     model_converter_ref mc = alloc(generic_model_converter, m, "dependent-expr-model");
-    // walk the trail from the back.
+    // walk the trail from the back
     // add substitutions from the back to the generic model converter
     // after they have been normalized using a global replace that replaces 
     // substituted variables by their terms.
