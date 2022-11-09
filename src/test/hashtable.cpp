15be80c9548f335527a39c011bdd19b03a0fe62d
Nikolaj Bjorner <nbjorner@microsoft.com>
2022-11-09 09:06:34 -0800

    remove dependency on hash_compare

--- src/test/hashtable.cpp
+++ src/test/hashtable.cpp
@@ -26,8 +26,8 @@ Revision History:
 
 struct int_hash_proc { unsigned operator()(int x) const { return x * 3; } };
 typedef int_hashtable<int_hash_proc, default_eq<int> > int_set;
-typedef std::unordered_set<int, std::hash_compare<int, std::less<int> > > safe_int_set;
 // typedef safe_int_set int_set;
+typedef std::unordered_set<int> safe_int_set;
 
 inline bool contains(int_set & h, int i) {
     // return h.find(i) != h.end();
