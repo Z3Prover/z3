42b4f6052a1891b6afa8a9a3215c1d0477fe8fa5
vincent-mirian-google <72002943+vincent-mirian-google@users.noreply.github.com>
2022-12-07 12:44:13 -0800

    Update mpz.cpp
    Modify the bound to avoid Undefined Behavior (UB).

--- src/util/mpz.cpp
+++ src/util/mpz.cpp
@@ -1460,7 +1460,7 @@ void mpz_manager<SYNCH>::bitwise_xor(mpz const & a, mpz const & b, mpz & c) {
 template<bool SYNCH>
 void mpz_manager<SYNCH>::bitwise_not(unsigned sz, mpz const & a, mpz & c) {
     SASSERT(is_nonneg(a));
-    if (is_small(a) && sz <= 63) {
+    if (is_small(a) && sz < 63) {
         int64_t mask = (static_cast<int64_t>(1) << sz) - static_cast<int64_t>(1);
         set_i64(c, (~ i64(a)) & mask);
     }
