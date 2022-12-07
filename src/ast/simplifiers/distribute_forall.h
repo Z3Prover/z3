c33e58ee1a4e482319de25a06c651dde75ed3ac6
Nikolaj Bjorner <nbjorner@microsoft.com>
2022-12-06 17:59:33 -0800

    update distribute forall
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/ast/simplifiers/distribute_forall.h
+++ src/ast/simplifiers/distribute_forall.h
@@ -15,31 +15,23 @@ Author:
 #pragma once
 
 #include "ast/simplifiers/dependent_expr_state.h"
-#include "ast/rewriter/distribute_forall.h"
 
 
 class distribute_forall_simplifier : public dependent_expr_simplifier {
-    distribute_forall m_dist;
+
+    struct rw_cfg;
+    struct rw;
     
 public:
+
     distribute_forall_simplifier(ast_manager& m, params_ref const& p, dependent_expr_state& fmls):
-        dependent_expr_simplifier(m, fmls),
-        m_dist(m) {
+        dependent_expr_simplifier(m, fmls) {
     }
 
     char const* name() const override { return "distribute-forall"; }
-        
-    void reduce() override {
-        if (!m_fmls.has_quantifiers())
-            return;
-        expr_ref r(m);
-        for (unsigned idx : indices()) {
-            auto const& d = m_fmls[idx];
-            if (!has_quantifiers(d.fml()))
-                continue;
-            m_dist(d.fml(), r);
-            m_fmls.update(idx, dependent_expr(m, r, nullptr, d.dep()));
-        }
-    }
+      
+    bool supports_proofs() const override { return true; }
+
+    void reduce() override;
 };
 
