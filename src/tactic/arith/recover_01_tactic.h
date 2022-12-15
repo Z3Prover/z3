aed3d76a886ea1ef0a7c2db9504df893e388e591
Nikolaj Bjorner <nbjorner@microsoft.com>
2022-12-14 16:45:58 -0800

    add doc
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/tactic/arith/recover_01_tactic.h
+++ src/tactic/arith/recover_01_tactic.h
@@ -5,29 +5,56 @@ Module Name:
 
     recover_01_tactic.h
 
-Abstract:
-
-    Recover 01 variables
-
-    Search for clauses of the form
-    p  or q or  x = 0
-    ~p or q or  x = k1
-    p  or ~q or x = k2
-    ~p or ~q or x = k1+k2
-
-    Then, replaces 
-    x with k1*y1 + k2*y2
-    p with y1=1
-    q with y2=1
-    where y1 and y2 are fresh 01 variables
-
-    The clauses are also removed.
-
 Author:
 
     Leonardo de Moura (leonardo) 2012-02-17.
 
-Revision History:
+Tactic Documentation:
+
+## Tactic recover-01
+
+### Short Description
+
+Recover 01 variables from propositional constants.
+
+### Long Description
+
+Search for clauses of the form
+
+```
+    p  or q or  x = 0
+    ~p or q or  x = k1
+    p  or ~q or x = k2
+    ~p or ~q or x = k1+k2
+```
+
+Then, replaces 
+
+
+* `x` with `k1*y1 + k2*y2`
+* `p` with `y1 = 1`
+* `q` with `y2 = 1`
+
+where `y1` and `y2` are fresh 01 variables.
+
+The clauses are also removed.
+
+### Example
+
+```z3
+(declare-const p Bool)
+(declare-const q Bool)
+(declare-const x Int)
+(assert (or p q (= x 0)))
+(assert (or (not p) q (= x 3)))
+(assert (or p (not q) (= x 6)))
+(assert (or (not p) (not q) (= x 9)))
+(apply recover-01)
+```
+
+### Notes
+
+* does not support proofs, does not support cores
 
 --*/
 #pragma once
