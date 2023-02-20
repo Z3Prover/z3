61ec3b9e87e03b62f6f12f43959fc2d8cf21b33f
Jakob Rath <git@jakobrath.eu>
2023-02-20 12:32:21 +0100

    log_lemma

--- src/math/polysat/saturation.cpp
+++ src/math/polysat/saturation.cpp
@@ -33,15 +33,13 @@ namespace polysat {
     saturation::saturation(solver& s) : s(s), m_lemma(s), m_parity_tracker(s) {}
 
     void saturation::log_lemma(pvar v, conflict& core) {
-        verbose_stream() << "Logging lemma:\n";
-        auto const& cl = core.lemmas().back();
-        verbose_stream() << m_rule << " v" << v << " ";
-        for (auto lit : *cl) verbose_stream() << s.lit2cnstr(lit) << " ";
-        verbose_stream() << " " << *cl << "\n";
-        IF_VERBOSE(1, auto const& cl = core.lemmas().back(); 
-                   verbose_stream() << m_rule << " v" << v << " "; 
-                   for (auto lit : *cl) verbose_stream() << s.lit2cnstr(lit) << " "; 
-                   verbose_stream() << " " << *cl << "\n");
+        IF_VERBOSE(1, {
+            auto const& cl = core.lemmas().back();
+            verbose_stream() << m_rule << " v" << v << " ";
+            for (auto lit : *cl)
+                verbose_stream() << s.lit2cnstr(lit) << " ";
+            verbose_stream() << " " << *cl << "\n";
+        });
     }
 
     void saturation::perform(pvar v, conflict& core) {
