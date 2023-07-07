8c7525c97fb5cd1d77cc0ce9b023eb51d8605f13
Nikolaj Bjorner <nbjorner@microsoft.com>
2023-07-07 09:29:38 -0700

    revert log addition
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/smt/qi_queue.cpp
+++ src/smt/qi_queue.cpp
@@ -399,7 +399,6 @@ namespace smt {
         TRACE("qi_queue", display_delayed_instances_stats(tout); tout << "lazy threshold: " << m_params.m_qi_lazy_threshold
               << ", scope_level: " << m_context.get_scope_level() << "\n";);
 
-        verbose_stream() << "delayed entries " << m_delayed_entries.size() << "\n";
         if (m_params.m_qi_conservative_final_check) {
             bool  init = false;
             float min_cost = 0.0;
