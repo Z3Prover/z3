8f605272f81c4aa8c2fe8aa8a6ac027904efab0e
Arie Gurfinkel <arie.gurfinkel@gmail.com>
2023-07-20 07:09:51 +0200

    array_peq: formatting, no change to code

--- src/ast/array_peq.h
+++ src/ast/array_peq.h
@@ -22,13 +22,12 @@ Revision History:
 #include "ast/array_decl_plugin.h"
 #include "ast/ast.h"
 
-
 /**
  * \brief utility class for partial equalities
  *
- * A partial equality (a ==I b), for two arrays a, b and a finite set of indices I holds
- * iff (forall i :: i \not\in I => a[i] == b[i]). In other words, peq is a
- * restricted form of the extensionality axiom
+ * A partial equality (a ==I b), for two arrays a, b and a finite set of indices
+ * I holds iff (forall i :: i \not\in I => a[i] == b[i]). In other words, peq is
+ * a restricted form of the extensionality axiom
  *
  * Using this class, we denote (a =I b) as f(a,b,i0,i1,...),
  * where f is an uninterpreted predicate with the name PARTIAL_EQ and
@@ -49,8 +48,8 @@ class peq {
   public:
     peq(app *p, ast_manager &m);
 
-    peq(expr *lhs, expr *rhs,
-        vector<expr_ref_vector> const &diff_indices, ast_manager &m);
+    peq(expr *lhs, expr *rhs, vector<expr_ref_vector> const &diff_indices,
+        ast_manager &m);
 
     expr_ref lhs() { return m_lhs; }
 
@@ -81,11 +80,12 @@ class peq {
  *
  * result has stores if either e0 or e1 or an index term has stores
  */
-app_ref mk_peq (expr* e0, expr* e1, vector<expr_ref_vector> const& indices, ast_manager& m);
+app_ref mk_peq(expr *e0, expr *e1, vector<expr_ref_vector> const &indices,
+               ast_manager &m);
 
-bool is_partial_eq (const func_decl* f);
+bool is_partial_eq(const func_decl *f);
 
-bool is_partial_eq (const app* a);
+bool is_partial_eq(const app *a);
 
 inline bool is_peq(const func_decl *f) { return is_partial_eq(f); }
 inline bool is_peq(const app *a) { return is_partial_eq(a); }
