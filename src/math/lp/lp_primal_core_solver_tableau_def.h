4d3956138bb5ff048b655e36f71a2623f43e09a6
Lev Nachmanson <levnach@hotmail.com>
2023-10-19 09:52:25 -0700

    set cancelled status on time over

--- src/math/lp/lp_primal_core_solver_tableau_def.h
+++ src/math/lp/lp_primal_core_solver_tableau_def.h
@@ -99,6 +99,7 @@ unsigned lp_primal_core_solver<T, X>::solve() {
         
     do {
         if (this->print_statistics_with_iterations_and_nonzeroes_and_cost_and_check_that_the_time_is_over( "feas t", * this->m_settings.get_message_ostream())) {
+            this->set_status(lp_status::CANCELLED);
             return this->total_iterations();
         }
         if (this->m_settings.use_tableau_rows()) {
