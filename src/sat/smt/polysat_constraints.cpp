4bcd2e038f1c4dfa07d0297476a2a7eadfab312e
Nikolaj Bjorner <nbjorner@microsoft.com>
2023-12-08 10:53:28 -0800

    port over ule_constraint

--- src/sat/smt/polysat_constraints.cpp
+++ src/sat/smt/polysat_constraints.cpp
@@ -11,15 +11,21 @@ Author:
     Jakob Rath 2021-04-06
 
 --*/
+
 #include "sat/smt/polysat_core.h"
 #include "sat/smt/polysat_solver.h"
 #include "sat/smt/polysat_constraints.h"
+#include "sat/smt/polysat_ule.h"
 
 namespace polysat {
 
     signed_constraint constraints::ule(pdd const& p, pdd const& q) {
+        pdd lhs = p, rhs = q;
+        bool is_positive = true;
+        ule_constraint::simplify(is_positive, lhs, rhs);
         auto* c = alloc(ule_constraint, p, q);
         m_trail.push(new_obj_trail(c));
-        return signed_constraint(ckind_t::ule_t, c);
+        auto sc = signed_constraint(ckind_t::ule_t, c);
+        return is_positive ? sc : ~sc;
     }
 }
