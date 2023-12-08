d14ab3d7075caf5627eb8a9de71eb913352d5c9e
Nikolaj Bjorner <nbjorner@microsoft.com>
2023-12-08 14:50:33 -0800

    porting viable

--- src/sat/smt/polysat_viable.h
+++ src/sat/smt/polysat_viable.h
@@ -17,7 +17,12 @@ Author:
 #pragma once
 
 #include "util/rational.h"
+#include "util/dlist.h"
+#include "util/map.h"
+#include "util/small_object_allocator.h"
+
 #include "sat/smt/polysat_types.h"
+#include "sat/smt/polysat_fi.h"
 
 namespace polysat {
 
@@ -29,26 +34,86 @@ namespace polysat {
     };
 
     class core;
+    class constraints;
 
     class viable {
         core& c;
-    public:
-        viable(core& c) : c(c) {}
+        constraints& cs;
+        forbidden_intervals      m_forbidden_intervals;
 
-	    /**
+        struct entry final : public dll_base<entry>, public fi_record {
+            /// whether the entry has been created by refinement (from constraints in 'fi_record::src')
+            bool refined = false;
+            /// whether the entry is part of the current set of intervals, or stashed away for backtracking
+            bool active = true;
+            bool valid_for_lemma = true;
+            pvar var = null_var;
+
+            void reset() {
+                // dll_base<entry>::init(this);  // we never did this in alloc_entry either
+                fi_record::reset();
+                refined = false;
+                active = true;
+                valid_for_lemma = true;
+                var = null_var;
+            }
+        };
+
+        enum class entry_kind { unit_e, equal_e, diseq_e };
+
+        struct layer final {
+            entry* entries = nullptr;
+            unsigned bit_width = 0;
+            layer(unsigned bw) : bit_width(bw) {}
+        };
+
+        class layers final {
+            svector<layer> m_layers;
+        public:
+            svector<layer> const& get_layers() const { return m_layers; }
+            layer& ensure_layer(unsigned bit_width);
+            layer* get_layer(unsigned bit_width);
+            layer* get_layer(entry* e) { return get_layer(e->bit_width); }
+            layer const* get_layer(unsigned bit_width) const;
+            layer const* get_layer(entry* e) const { return get_layer(e->bit_width); }
+            entry* get_entries(unsigned bit_width) const { layer const* l = get_layer(bit_width); return l ? l->entries : nullptr; }
+        };
+
+        ptr_vector<entry>       m_alloc;
+        vector<layers>          m_units;        // set of viable values based on unit multipliers, layered by bit-width in descending order
+        ptr_vector<entry>       m_equal_lin;    // entries that have non-unit multipliers, but are equal
+        ptr_vector<entry>       m_diseq_lin;    // entries that have distinct non-zero multipliers
+
+        entry* alloc_entry(pvar v);
+
+        std::ostream& display_one(std::ostream& out, pvar v, entry const* e) const;
+        std::ostream& display_all(std::ostream& out, pvar v, entry const* e, char const* delimiter = "") const;
+        void log();
+        void log(pvar v);
+
+        void insert(entry* e, pvar v, ptr_vector<entry>& entries, entry_kind k);
+
+        void intersect(pvar v, entry* e);
+
+
+    public:
+        viable(core& c);
+        ~viable();
+
+        /**
          * Find a next viable value for variable.
          */
-        find_t find_viable(pvar v, rational& out_val) { throw default_exception("nyi"); }
+        find_t find_viable(pvar v, rational& out_val);
 
         /*
         * Explain why the current variable is not viable or signleton.
         */
-        dependency_vector explain() { throw default_exception("nyi"); }
+        dependency_vector explain();
 
         /*
         * Register constraint at index 'idx' as unitary in v.
         */
-        void add_unitary(pvar v, unsigned idx) { throw default_exception("nyi"); }
+        void add_unitary(pvar v, unsigned idx);
 
     };
 
