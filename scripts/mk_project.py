133546625c411bb174c66b58341b534713331592
Nikolaj Bjorner <nbjorner@microsoft.com>
2024-01-24 09:24:46 -0800

    update minor version number
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- scripts/mk_project.py
+++ scripts/mk_project.py
@@ -8,7 +8,7 @@
 from mk_util import *
 
 def init_version():
-    set_version(4, 12, 5, 0) # express a default build version or pick up ci build version
+    set_version(4, 12, 6, 0) # express a default build version or pick up ci build version
     
 # Z3 Project definition
 def init_project_def():
