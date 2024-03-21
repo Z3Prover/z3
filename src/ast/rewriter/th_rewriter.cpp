6455de9dd32b0bb75e86383cda0d8f172cb6ef7d
Nikolaj Bjorner <nbjorner@microsoft.com>
2024-03-21 09:39:13 -0700

    fix #7179
    Ensure that flat associative rewriting is disabled if rewriter.flat is set to false.

--- src/ast/rewriter/th_rewriter.cpp
+++ src/ast/rewriter/th_rewriter.cpp
@@ -17,6 +17,7 @@ Notes:
 
 --*/
 #include "params/rewriter_params.hpp"
+#include "params/poly_rewriter_params.hpp"
 #include "ast/rewriter/th_rewriter.h"
 #include "ast/rewriter/bool_rewriter.h"
 #include "ast/rewriter/arith_rewriter.h"
@@ -83,7 +84,8 @@ struct th_rewriter_cfg : public default_rewriter_cfg {
 
     void updt_local_params(params_ref const & _p) {
         rewriter_params p(_p);
-        m_flat           = true;
+        poly_rewriter_params pp(_p);
+        m_flat           = pp.flat();
         m_max_memory     = megabytes_to_bytes(p.max_memory());
         m_max_steps      = p.max_steps();
         m_pull_cheap_ite = p.pull_cheap_ite();
