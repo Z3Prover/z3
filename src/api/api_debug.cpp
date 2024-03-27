eae6814fce3c783b1a8adfcc3a5c5f9c05d93a43
Steven Moy <github@stevenmoy.com>
2024-03-27 10:10:15 -0700

    Turn on exit_action_to_throw_exception upon API context creation

--- src/api/api_debug.cpp
+++
@@ -1,8 +0,0 @@
-#include "api/z3.h"
-#include "util/util.h"
-
-extern "C" {
-    void Z3_API Z3_set_exit_action_to_throw_exception() {
-        set_default_exit_action(exit_action::throw_exception);
-    }
-}
