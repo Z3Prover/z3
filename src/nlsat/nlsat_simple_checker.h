33f0256e206a8f7d02ed9d5159492ee69f6a45b3
Lev Nachmanson <levnach@hotmail.com>
2024-08-10 14:53:56 -1000

    cleanup

--- src/nlsat/nlsat_simple_checker.h
+++ src/nlsat/nlsat_simple_checker.h
@@ -7,7 +7,6 @@ namespace nlsat {
         struct imp;
         imp *  m_imp;
     public:
-        // Simple_Checker(solver &_sol, pmanager &_pm, anum_manager &_am, const clause_vector &_clauses, clause_vector &_learned, const atom_vector &_atoms, const unsigned &_arith_var_num);
         Simple_Checker(pmanager &_pm, anum_manager &_am, const clause_vector &_clauses, literal_vector &_learned_unit, const atom_vector &_atoms, const unsigned &_arith_var_num);
         ~Simple_Checker();
         bool operator()();
