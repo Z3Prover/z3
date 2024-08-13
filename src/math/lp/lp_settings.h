7050589adfd6fede36f1a7bb24b228853c7815f2
Lev Nachmanson <levnach@hotmail.com>
2024-08-13 13:20:45 -1000

    prepare for calling diophantine equations

--- src/math/lp/lp_settings.h
+++ src/math/lp/lp_settings.h
@@ -236,7 +236,7 @@ public:
     void set_hnf_cut_period(unsigned period) { m_hnf_cut_period = period;  }
     unsigned random_next() { return m_rand(); }
     unsigned random_next(unsigned u ) { return m_rand(u); }
-    
+    bool dioph_eq() { return m_dioph_eq; }
     void set_random_seed(unsigned s) { m_rand.set_seed(s); }
 
     bool bound_progation() const { 
