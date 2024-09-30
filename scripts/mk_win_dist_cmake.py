b65afd41ed8c157168ca157e0b31d9d37290b48f
Nikolaj Bjorner <nbjorner@microsoft.com>
2024-09-30 08:58:21 -0700

    attempt to use uniform java library location under bin #7406

--- scripts/mk_win_dist_cmake.py
+++ scripts/mk_win_dist_cmake.py
@@ -376,9 +376,10 @@ def cp_into_bin(arch):
     shutil.rmtree(lib_dir)
     if JAVA_ENABLED:
         java_dir = get_java_dist_path(arch)
-        shutil.copytree(java_dir,
-                        bin_dir,
-                        dirs_exist_ok=True)
+        for file in os.listdir(java_dir):
+            src_path = os.path.join(java_dir, file)
+            dst_path = os.path.join(bin_dir, file)
+            shutil.copy2(src_path, dst_path)
         shutil.rmtree(java_dir)
                 
 def cp_pdb(arch):
