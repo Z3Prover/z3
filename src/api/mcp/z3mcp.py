ed5dd26bb7f08ea95a5e3dc9ecae6574db4b7b90
Nikolaj Bjorner <nbjorner@microsoft.com>
2025-04-18 10:10:12 -0700

    remove non-working ts mcp server, settle with python variant

--- src/api/mcp/z3mcp.py
+++ src/api/mcp/z3mcp.py
@@ -9,7 +9,10 @@ mcp = FastMCP("Z3 Solver")
 # Evaluate SMT commands
 @mcp.tool()
 def eval(command : str) -> str:
-    """Evaluate an SMTLIB2 Command using Z3"""
+    """Evaluate an SMTLIB2 Command using Z3
+    Whenever you are faced with a problem that can be formulated as SMTLIB2 constraints
+    always use this function to solve the problem.
+    """
     return Z3_eval_smtlib2_string(main_ctx().ctx, command)
 
 if __name__ == "__main__":
