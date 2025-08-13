6ef47430152516cc16ec5c9144009eb880d6225a
Lev Nachmanson <levnach@hotmail.com>
2025-08-13 13:34:42 -0700

    more scaffolding
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- src/nlsat/levelwise.h
+++ src/nlsat/levelwise.h
@@ -3,24 +3,22 @@
 
 namespace nlsat {
 
-class assignment; // forward declared in nlsat_types.h
+    class assignment; // forward declared in nlsat_types.h
+    struct symbolic_interval {};
 
-// Levelwise driver (PIMPL). Orchestrates property-based projection/proof.
-class levelwise {
-    struct impl;
-    impl* m_impl;
-public:
-    // Construct with polynomials ps, maximal variable max_x, and current sample s (assignment of vars < max_x)
-    levelwise(polynomial_ref_vector const& ps, var max_x, assignment const& s);
-    ~levelwise();
+    class levelwise {
+        struct impl;
+        impl* m_impl;
+    public:
+        // Construct with polynomials ps, maximal variable max_x, and current sample s (assignment of vars < max_x)
+        levelwise(polynomial_ref_vector const& ps, var max_x, assignment const& s);
+        ~levelwise();
 
-    levelwise(levelwise const&) = delete;
-    levelwise& operator=(levelwise const&) = delete;
-    levelwise(levelwise&&) noexcept;
-    levelwise& operator=(levelwise&&) noexcept;
-};
+        levelwise(levelwise const&) = delete;
+        levelwise& operator=(levelwise const&) = delete;
+        std::vector<symbolic_interval> single_cell();        
+    };
 
-// Convenience free-function driver prototype
-polynomial_ref_vector levelwise_project(polynomial_ref_vector& ps, var max_x, assignment const& s);
+    //
 
 } // namespace nlsat
