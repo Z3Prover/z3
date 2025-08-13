bbd304e1a545d16685a80696dd1d71a6ececda2b
Lev Nachmanson <levnach@hotmail.com>
2025-08-12 18:25:20 -0700

    scaffolding
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- src/nlsat/levelwise.h
+++ src/nlsat/levelwise.h
@@ -1,6 +1,26 @@
 #pragma once
 #include "nlsat_types.h"
+
 namespace nlsat {
-    void lewelwise_project(polynomial_ref_vector & ps, var max_x);
+
+class assignment; // forward declared in nlsat_types.h
+
+// Levelwise driver (PIMPL). Orchestrates property-based projection/proof.
+class levelwise {
+    struct impl;
+    impl* m_impl;
+public:
+    // Construct with polynomials ps, maximal variable max_x, and current sample s (assignment of vars < max_x)
+    levelwise(polynomial_ref_vector const& ps, var max_x, assignment const& s);
+    ~levelwise();
+
+    levelwise(levelwise const&) = delete;
+    levelwise& operator=(levelwise const&) = delete;
+    levelwise(levelwise&&) noexcept;
+    levelwise& operator=(levelwise&&) noexcept;
+};
+
+// Convenience free-function driver prototype
+void levelwise_project(polynomial_ref_vector const& ps, var max_x, assignment const& s);
 
 } // namespace nlsat
