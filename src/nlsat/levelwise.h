1066f3ea3ba7053a22c05b08331d322309d8c056
Lev Nachmanson <levnach@hotmail.com>
2025-08-14 16:24:58 -0700

    pass nlsat::solver to levelwise
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- src/nlsat/levelwise.h
+++ src/nlsat/levelwise.h
@@ -34,7 +34,7 @@ namespace nlsat {
         impl* m_impl;
     public:
     // Construct with polynomials ps, maximal variable max_x, current sample s, polynomial manager pm, and algebraic-number manager am
-    levelwise(polynomial_ref_vector const& ps, var max_x, assignment const& s, pmanager& pm, anum_manager& am);
+        levelwise(nlsat::solver& solver, polynomial_ref_vector const& ps, var max_x, assignment const& s, pmanager& pm, anum_manager& am);
         ~levelwise();
 
         levelwise(levelwise const&) = delete;
