35fac9c578cce96c61bcf1ef90a7ec84ba292e0c
Lev Nachmanson <levnach@hotmail.com>
2025-09-27 07:22:33 -0800

    remove a parameter
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- src/nlsat/nlsat_explain.cpp
+++ src/nlsat/nlsat_explain.cpp
@@ -1135,7 +1135,7 @@ namespace nlsat {
             
             polynomial_ref_vector samples(m_pm);
             if (x < max_x)
-                cac_add_cell_lits(ps, x, samples);
+                cac_add_cell_lits(ps, x);
 
             while (true) {
                 if (all_univ(ps, x) && m_todo.empty()) {
@@ -1160,7 +1160,7 @@ namespace nlsat {
                 if (m_todo.empty())
                     break;
                 x = m_todo.extract_max_polys(ps);
-                cac_add_cell_lits(ps, x, samples);
+                cac_add_cell_lits(ps, x);
             }
         }
 
