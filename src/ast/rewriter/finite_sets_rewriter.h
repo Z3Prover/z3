522be5d8c2598e69f344f9f359c76eec518230ee
Nikolaj Bjorner <nbjorner@microsoft.com>
2025-10-04 18:14:43 -0700

    add stub for rewriter
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

---
+++ src/ast/rewriter/finite_sets_rewriter.h
@@ -0,0 +1,20 @@
+/*++
+Copyright (c) 2025 Microsoft Corporation
+
+Module Name:
+
+    finite_sets_rewriter.h
+
+Abstract:
+    Rewriting Simplification for finite sets
+
+
+Sampe rewrite rules:
+    set.union s set.empty -> s
+    set.intersect s set.empty -> set.empty
+    set.in x (set.singleton y) -> x = y
+
+Generally this module implements basic algebraic simplificaiton rules for finite sets
+where the signature is defined in finite_sets_decl_plugin.h.
+   
+--*/
