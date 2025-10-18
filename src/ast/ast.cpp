6fa12312b3b0822fd88dc452a332c31153bd69f4
Nikolaj Bjorner <nbjorner@microsoft.com>
2025-10-18 13:30:46 +0200

    fix build
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/ast/ast.cpp
+++ src/ast/ast.cpp
@@ -1715,7 +1715,6 @@ ast * ast_manager::register_node_core(ast * n) {
 
     n->m_id = is_decl(n) ? m_decl_id_gen.mk() : m_expr_id_gen.mk();        
 
-  //  track_id(*this, n, 715);
     
 //    TRACE(ast, tout << (s_count++) << " Object " << n->m_id << " was created.\n";);
     TRACE(mk_var_bug, tout << "mk_ast: " << n->m_id << "\n";);
