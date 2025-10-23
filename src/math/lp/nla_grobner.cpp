58e64ea8264b42feb5a9c824bd4f3944aed65616
Lev Nachmanson <levnach@hotmail.com>
2025-10-22 17:00:16 -0700

    try exponential delay in grobner
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- src/math/lp/nla_grobner.cpp
+++ src/math/lp/nla_grobner.cpp
@@ -47,9 +47,22 @@ namespace nla {
         if (m_quota == 0)
             m_quota = c().params().arith_nl_gr_q();                    
 
+        bool const use_exp_delay = c().params().arith_nl_grobner_exp_delay();
+
         if (m_quota == 1) {
-            m_delay_base++;
-            m_delay = m_delay_base;
+            if (use_exp_delay) {
+                constexpr unsigned delay_cap = 1000000;
+                if (m_delay_base == 0)
+                    m_delay_base = 1;
+                else if (m_delay_base < delay_cap) {
+                    m_delay_base *= 2;
+                    if (m_delay_base > delay_cap)
+                        m_delay_base = delay_cap;
+                }
+                m_delay = m_delay_base;
+            }
+            else
+                m_delay = ++m_delay_base;
             m_quota = c().params().arith_nl_gr_q();
         }
 
