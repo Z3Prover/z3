543a473993db14c1e9f1214e74544776d21ad82e
Lev Nachmanson <5377127+levnach@users.noreply.github.com>
2025-12-16 17:55:26 -1000

    Update nlsat_explain.cpp
    Remove a duplicate call

--- src/nlsat/nlsat_explain.cpp
+++ src/nlsat/nlsat_explain.cpp
@@ -457,8 +457,7 @@ namespace nlsat {
                 TRACE(nlsat_explain, tout << "lc: " << lc << " reduct: " << reduct << "\n";);
                 insert_fresh_factors_in_todo(lc);
                 if (!is_zero(lc) && sign(lc)) {
-                    insert_fresh_factors_in_todo(lc);
-                    TRACE(nlsat_explain, tout << "lc does no vaninsh\n";);
+                    TRACE(nlsat_explain, tout << "lc does no vanish\n";);
                     return;
                 }
                 add_zero_assumption(lc);
