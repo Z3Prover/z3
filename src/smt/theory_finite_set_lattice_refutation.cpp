b9f5b301146983adca890d5d2ce463666d293641
Lorenz Winkler <lorenz.winkler@tuwien.ac.at>
2026-01-08 12:02:34 +0100

    begin with the incremental reachability data structure

--- src/smt/theory_finite_set_lattice_refutation.cpp
+++ src/smt/theory_finite_set_lattice_refutation.cpp
@@ -4,9 +4,11 @@
 #include "smt/theory_finite_set.h"
 #include "smt/smt_context.h"
 
+const int INITIAL_MAX_DIMENSION = 1000;
+
 namespace smt {
     theory_finite_set_lattice_refutation::theory_finite_set_lattice_refutation(theory_finite_set& th): 
-    m(th.m), ctx(th.ctx), th(th), u(m), bs(m), m_assumption(m) {}
+    m(th.m), ctx(th.ctx), th(th), u(m), bs(m), m_assumption(m), reachability(INITIAL_MAX_DIMENSION) {}
 
     // determines if the two enodes capture a subset relation:
     // checks, whether intersect_expr = intersect(subset, return_value) for some return value
@@ -38,8 +40,9 @@ namespace smt {
             return;
         }
         relations.push_back({subset, superset});
+        ctx.push_trail(push_back_trail(relations));
         TRACE(finite_set, tout << "new_eq_intersection: " << enode_pp(subset, ctx) << "\\subseteq " << enode_pp(superset, ctx));
-            
+        eq_propagation_justification(n1, n2);
     };
 
     void theory_finite_set_lattice_refutation::add_disequality(theory_var v1, theory_var v2){
@@ -56,12 +59,21 @@ namespace smt {
             return;
         }
         non_relations.push_back({subset, superset});
+        ctx.push_trail(push_back_trail(non_relations));
         TRACE(finite_set, tout << "new_diseq_intersection: " << enode_pp(subset, ctx) << "\\not\\subseteq " << enode_pp(superset, ctx));
 
         auto eq_expr = m.mk_not(m.mk_eq(n1->get_expr(), n2->get_expr()));
-        auto is_true = ctx.get_assignment(eq_expr);
-        TRACE(finite_set, tout << "new_diseq_intersection_assignment: " << is_true);
+        auto lit = ctx.get_literal(eq_expr);
+        TRACE(finite_set, tout << "new_diseq_intersection_assignment: " << lit);
         ctx.display_assignment(tout);
+    }
 
+    void theory_finite_set_lattice_refutation::check_conflict(){
+        for (size_t i = 0; i < non_relations.size(); i++)
+        {
+            auto rel = non_relations[i];
+            vector<parameter> params;
+            auto just = ctx.mk_justification(ext_theory_conflict_justification(th.get_id(), ctx, 0, nullptr, 0, nullptr, params.size(), params.data()));
+        }
     }
 }
--- src/smt/theory_finite_set_lattice_refutation.h
+++ src/smt/theory_finite_set_lattice_refutation.h
@@ -8,6 +8,30 @@ namespace smt {
     class context;
     class theory_finite_set;
 
+    class reachability_matrix{
+        // this class is a 2d matrix with a fixed initial size (to avoid reallocation) which 
+        std::vector<int> is_related;
+
+        // this stores the equalities (intersections), that justify the subset relations.
+        std::vector<std::pair<theory_var, enode_pair>*> subset_relations;
+        unsigned m_max_cols;
+
+        unsigned m_rows;
+        unsigned m_cols;
+
+        context& ctx;
+
+        public:
+            reachability_matrix(unsigned max_dim);
+            bool is_reachable(theory_var source, theory_var dest);
+            // get_reachability_reason(i,j) returns:
+            //   - {-1, equality} when equality covers the subset relation of i and j
+            //   - {intermediate, equality} when intermediate is a subset of j, and equality 
+            //     covers the relation of i and intermediate (transitivity)
+            std::pair<theory_var, enode_pair> get_reachability_reason(theory_var source, theory_var dest);
+            int set(theory_var source, theory_var dest, int value, theory_var intermediate, enode_pair subset_relation);
+    };
+
     class theory_finite_set_lattice_refutation {
         ast_manager          &m;
         context             &ctx;
@@ -15,10 +39,12 @@ namespace smt {
         finite_set_util u;
         expr_ref_vector bs;
         expr_ref m_assumption;
+        reachability_matrix reachability;
 
         svector<std::pair<enode*, enode*>> relations;
         svector<std::pair<enode*, enode*>> non_relations;
         enode* get_superset(enode*, enode*);
+        void check_conflict();
         public:
             theory_finite_set_lattice_refutation(theory_finite_set &th);
             void add_equality(theory_var v1, theory_var v2);
