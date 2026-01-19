40efe2706663e50c1466270067f60d9225c403ad
Nikolaj Bjorner <nbjorner@microsoft.com>
2026-01-18 17:48:20 -0800

    add formatting
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/smt/theory_finite_set_lattice_refutation.h
+++ src/smt/theory_finite_set_lattice_refutation.h
@@ -1,3 +1,12 @@
+/*++
+Copyright (c) 2025 Lorenz Winkler
+
+Module Name:
+
+    theory_finite_lattice_refutation.h
+
+--*/
+
 #pragma once
 
 #include "ast/finite_set_decl_plugin.h"
@@ -9,8 +18,7 @@ namespace smt {
     class theory_finite_set;
 
     class theory_finite_set_lattice_refutation;
-    class reachability_matrix{
-
+    class reachability_matrix {
         std::vector<uint64_t> reachable;
         std::vector<enode_pair> links;
         std::vector<uint64_t> link_dls;
@@ -21,8 +29,8 @@ namespace smt {
 
         int max_size;
 
-        context& ctx;
-        theory_finite_set_lattice_refutation& t_lattice_refutation;
+        context &ctx;
+        theory_finite_set_lattice_refutation &t_lattice_refutation;
         int conflict_row = -1;
         int conflict_word = -1;
 
@@ -30,42 +38,43 @@ namespace smt {
         bool bitwise_or_rows(int source_dest, int source);
         inline int get_word_index(int row, int col) const;
         inline uint64_t get_bitmask(int col) const;
-        public:
-            void get_path(theory_var source, theory_var dest, vector<enode_pair>& path, int& num_decisions);
-            reachability_matrix(context& ctx, theory_finite_set_lattice_refutation& t_lattice);
-            bool in_bounds(theory_var source, theory_var dest);
-            bool is_reachable(theory_var source, theory_var dest);
-            bool is_reachability_forbidden(theory_var source, theory_var dest);
-            bool is_linked(theory_var source, theory_var dest);
 
-            bool check_reachability_conflict(theory_var source, theory_var dest);
-            bool check_reachability_conflict_word(int row, int word);
+    public:
+        void get_path(theory_var source, theory_var dest, vector<enode_pair> &path, int &num_decisions);
+        reachability_matrix(context &ctx, theory_finite_set_lattice_refutation &t_lattice);
+        bool in_bounds(theory_var source, theory_var dest);
+        bool is_reachable(theory_var source, theory_var dest);
+        bool is_reachability_forbidden(theory_var source, theory_var dest);
+        bool is_linked(theory_var source, theory_var dest);
 
-            bool set_reachability(theory_var source, theory_var dest, enode_pair reachability_witness);
-            bool set_non_reachability(theory_var source, theory_var dest, enode_pair non_reachability_witness);
-            int get_max_var();
-            void print_relations();
-            
+        bool check_reachability_conflict(theory_var source, theory_var dest);
+        bool check_reachability_conflict_word(int row, int word);
+
+        bool set_reachability(theory_var source, theory_var dest, enode_pair reachability_witness);
+        bool set_non_reachability(theory_var source, theory_var dest, enode_pair non_reachability_witness);
+        int get_max_var();
+        void print_relations();
     };
 
     class theory_finite_set_lattice_refutation {
-        ast_manager          &m;
-        context             &ctx;
-        theory_finite_set   &th;
+        ast_manager &m;
+        context &ctx;
+        theory_finite_set &th;
         finite_set_util u;
         expr_ref_vector bs;
         expr_ref m_assumption;
         reachability_matrix reachability;
 
-        enode* get_superset(enode*, enode*);
+        enode *get_superset(enode *, enode *);
         void add_subset(theory_var subset, theory_var superset, enode_pair justifying_equality);
         void add_not_subset(theory_var subset, theory_var superset, enode_pair justifying_disequality);
         void propagate_new_subset(theory_var v1, theory_var v2);
-        void add_set_equality(enode* set1, enode* set2);
-        public:
-            void trigger_conflict(vector<enode_pair> equalities, enode_pair clashing_disequality);
-            theory_finite_set_lattice_refutation(theory_finite_set &th);
-            void add_equality(theory_var v1, theory_var v2);
-            void add_disequality(theory_var v1, theory_var v2);
+        void add_set_equality(enode *set1, enode *set2);
+
+    public:
+        void trigger_conflict(vector<enode_pair> equalities, enode_pair clashing_disequality);
+        theory_finite_set_lattice_refutation(theory_finite_set &th);
+        void add_equality(theory_var v1, theory_var v2);
+        void add_disequality(theory_var v1, theory_var v2);
     };
-}
+}  // namespace smt
