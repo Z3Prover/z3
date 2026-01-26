382b3d9a970cf7ea3312280e503467371628b4bc
Nikolaj Bjorner <nbjorner@microsoft.com>
2026-01-26 10:12:05 -0800

    Reset timestamp for variables in sat_ddfw.cpp

--- src/ast/sls/sat_ddfw.cpp
+++ src/ast/sls/sat_ddfw.cpp
@@ -220,6 +220,7 @@ namespace sat {
         add_assumptions();
         for (unsigned v = 0; v < num_vars(); ++v) {
             value(v) = (m_rand() % 2) == 0; // m_use_list[lit.index()].size() >= m_use_list[nlit.index()].size();
+            m_vars[v].m_timestamp = 0;
         }
 
         if (!flatten_use_list())
