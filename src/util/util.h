c008b7b22814072e2a37f0610d015cbac0f8d32a
Nikolaj Bjorner <nbjorner@microsoft.com>
2026-01-25 21:10:02 -0800

    fix underflow bug
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/util/util.h
+++ src/util/util.h
@@ -363,7 +363,7 @@ public:
 template<typename T>
 void shuffle(std::span<T> array, random_gen & gen) {
     auto n = array.size();
-    while (--n > 0) {
+    while (n-- > 0) {
         int k = gen() % (n + 1);
         std::swap(array[n], array[k]);
     }
