8adb3a031ecec8186c7c3e273f27bb0a5fe60561
Nuno Lopes <nuno.lopes@tecnico.ulisboa.pt>
2026-01-30 15:26:21 +0000

    code simplifications

--- src/util/common_msgs.cpp
+++ src/util/common_msgs.cpp
@@ -18,10 +18,10 @@ Notes:
 --*/
 #include "util/common_msgs.h"
 
-char const * common_msgs::g_canceled_msg   = "canceled";
-char const * common_msgs::g_max_memory_msg = "max. memory exceeded";
-char const * common_msgs::g_max_scopes_msg = "max. scopes exceeded";
-char const * common_msgs::g_max_steps_msg  = "max. steps exceeded";
-char const * common_msgs::g_max_frames_msg = "max. frames exceeded";
-char const * common_msgs::g_no_proofs_msg  = "component does not support proof generation";
-char const * common_msgs::g_max_resource_msg = "max. resource limit exceeded";
+char const common_msgs::g_canceled_msg[]   = "canceled";
+char const common_msgs::g_max_memory_msg[] = "max. memory exceeded";
+char const common_msgs::g_max_scopes_msg[] = "max. scopes exceeded";
+char const common_msgs::g_max_steps_msg[]  = "max. steps exceeded";
+char const common_msgs::g_max_frames_msg[] = "max. frames exceeded";
+char const common_msgs::g_no_proofs_msg[]  = "component does not support proof generation";
+char const common_msgs::g_max_resource_msg[] = "max. resource limit exceeded";
