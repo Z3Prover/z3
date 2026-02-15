f9ac2539df675e1b488a26b7cbd596f77300e8df
Nuno Lopes <nuno.lopes@tecnico.ulisboa.pt>
2026-02-15 16:20:44 +0000

    fix

--- src/util/rational.h
+++ src/util/rational.h
@@ -50,7 +50,7 @@ public:
 
     explicit rational(unsigned n) { m().set(m_val, n); }
       
-    rational(int n, int d) { m().set(m_val, n, d); }
+    rational(int64_t n, int d) { m().set(m_val, n, d); }
     rational(mpq const & q) { m().set(m_val, q); }
     rational(mpq && q) noexcept : m_val(std::move(q)) {}
     rational(mpz const & z) { m().set(m_val, z); }
