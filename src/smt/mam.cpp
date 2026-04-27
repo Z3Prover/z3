a76b5644ed48772f6d5b7ec8abe43af51f1bb717
CanCebeci <can.cebeci@epfl.ch>
2026-04-27 10:59:45 +0200

    Tidy up get_*_f_app

--- src/smt/mam.cpp
+++ src/smt/mam.cpp
@@ -1882,56 +1882,40 @@ namespace {
                 m_used_enodes.push_back(std::make_tuple(prev, n));
         }
 
+        void get_f_app(func_decl* lbl, unsigned num_expected_args, enode* curr, enode*& matching_cgr, enode*& min_gen_match) {
+            if (curr->get_decl() == lbl && curr->get_num_args() == num_expected_args) {
+                if (curr->is_cgr() && !matching_cgr)
+                    matching_cgr = curr;
+                if (!min_gen_match || min_gen_match->get_generation() > curr->get_generation())
+                    min_gen_match = curr;
+            }
+        }
+
         // We have to provide the number of expected arguments because we have flat-assoc applications such as +.
         // Flat-assoc applications may have arbitrary number of arguments.
         enode * get_first_f_app(func_decl * lbl, unsigned num_expected_args, enode * curr) {
             enode * first = curr;
-            enode * matching_cgr = nullptr;
-            enode * min_gen_match = nullptr;
+            enode *matching_cgr = nullptr, *min_gen_match = nullptr;
             do {
-                if (curr->get_decl() == lbl  && curr->get_num_args() == num_expected_args) {
-                    if (matching_cgr == nullptr && curr->is_cgr()) {
-                        matching_cgr = curr;
-                    }
-                    // To compute the generation the match occurs at, use the minimum generation number among all matching terms in the congruence class.
-                    // This ensures that the generation number corresponds to the shortest path of instantiations. 
-                    if (min_gen_match == nullptr || min_gen_match->get_generation() > curr->get_generation()) {
-                        min_gen_match = curr;
-                    }
-                }
+                get_f_app(lbl, num_expected_args, curr, matching_cgr, min_gen_match);
                 curr = curr->get_next();
             }
             while (curr != first);
-
-            if (matching_cgr != nullptr) {
-                update_max_generation(min_gen_match, first);
-                return matching_cgr;
-            }
-
-            return nullptr;
+            if (matching_cgr)
+                update_max_generation(min_gen_match, first);                          
+            return matching_cgr;
         }
 
         enode * get_next_f_app(func_decl * lbl, unsigned num_expected_args, enode * first, enode * curr) {
             curr = curr->get_next();
-            enode * matching_cgr = nullptr;
-            enode * min_gen_match = nullptr;
+            enode *matching_cgr = nullptr, *min_gen_match = nullptr;
             while (curr != first) {
-                if (curr->get_decl() == lbl  && curr->get_num_args() == num_expected_args) {
-                    if (matching_cgr == nullptr && curr->is_cgr()) {
-                        matching_cgr = curr;
-                    }
-                    if (min_gen_match == nullptr || min_gen_match->get_generation() > curr->get_generation()) {
-                        min_gen_match = curr;
-                    }
-                }
+                get_f_app(lbl, num_expected_args, curr, matching_cgr, min_gen_match);
                 curr = curr->get_next();
             }
-            if (matching_cgr != nullptr) {
+            if (matching_cgr)
                 update_max_generation(min_gen_match, first);
-                return matching_cgr;
-            }
-
-            return nullptr;
+            return matching_cgr;
         }
 
 
