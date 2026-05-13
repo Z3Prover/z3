09b75d2122077c75bf6e4b1c3c3a1b9a0b9f5dc3
Nikolaj Bjorner <nbjorner@microsoft.com>
2026-05-13 14:58:55 -0700

    connect parallel tactical2 as side load
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/sat/sat_solver/inc_sat_solver.cpp
+++ src/sat/sat_solver/inc_sat_solver.cpp
@@ -27,6 +27,7 @@ Notes:
 #include "solver/tactic2solver.h"
 #include "solver/parallel_params.hpp"
 #include "solver/parallel_tactical.h"
+#include "solver/parallel_tactical2.h"
 #include "tactic/tactical.h"
 #include "tactic/aig/aig_tactic.h"
 #include "tactic/core/propagate_values_tactic.h"
@@ -1183,5 +1184,9 @@ void inc_sat_display(std::ostream& out, solver& _s, unsigned sz, expr*const* sof
 
 tactic * mk_psat_tactic(ast_manager& m, params_ref const& p) {
     parallel_params pp(p);
-    return pp.enable() ? mk_parallel_tactic(mk_inc_sat_solver(m, p, false), p) : mk_sat_tactic(m);
+    if (pp.enable())
+        return mk_parallel_tactic(mk_inc_sat_solver(m, p, false), p);
+    if (pp.enable2())
+        return mk_parallel_tactic2(mk_inc_sat_solver(m, p, false), p);
+    return mk_sat_tactic(m);
 }
