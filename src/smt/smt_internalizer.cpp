90db6ca50d89de3498ccaebc543c91c44b14b0ec
Can Cebeci <t-cancebeci@microsoft.com>
2026-05-22 13:07:27 -0700

    Make ctx argument optional

--- src/smt/smt_internalizer.cpp
+++ src/smt/smt_internalizer.cpp
@@ -103,7 +103,7 @@ namespace smt {
 
     void context::update_generation(enode * e) {
         if (0 < m_generation && m_generation < e->get_generation())
-            e->set_generation(*this, m_generation);
+            e->set_generation(nullptr, m_generation);
     }
 
     void context::ts_visit_child(expr * n, bool gate_ctx, svector<expr_bool_pair> & todo, bool & visited) {
