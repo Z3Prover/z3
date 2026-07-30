4162b70a8aff7e58bb5d40deccdeb48c0bfef4d5
Nikolaj Bjorner <nikolaj@cs.stanford.edu>
2026-07-30 11:42:25 -0700

    Fix propagation logic in nla_core.cpp

--- src/math/lp/nla_core.cpp
+++ src/math/lp/nla_core.cpp
@@ -1530,10 +1530,12 @@ void core::set_use_nra_model(bool m) {
     
 bool core::propagate() {
     clear();
+	bool propagated = false;
     if (m_monomial_bounds.propagate_fixed_rows())
-        m_check_feasible = true;
-    bool propagated = m_monomial_bounds.tighten_lp_bounds();
-    if (m_monomial_bounds.propagate_changed_bounds())
+        propagated = true;
+    if (m_monomial_bounds.tighten_lp_bounds())
+		propgated = true;
+    if (m_monomial_bounds.propagate_changed_bounds()) 
         propagated = true;
     m_monics_with_changed_bounds.reset();
     if (propagated)
