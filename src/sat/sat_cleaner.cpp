dece18a60a4363fc97b856989efa4bae2e1da36f
Nikolaj Bjorner <nikolaj@cs.stanford.edu>
2026-07-30 19:34:58 -0700

    Fix verbose output in sat_cleaner.cpp

--- src/sat/sat_cleaner.cpp
+++ src/sat/sat_cleaner.cpp
@@ -166,7 +166,7 @@ namespace sat {
                        verbose_stream() << " (sat-cleaner";
                        verbose_stream() << " :elim-literals " << (m_cleaner.m_elim_literals - m_elim_literals);
                        verbose_stream() << " :elim-clauses " << (m_cleaner.m_elim_clauses - m_elim_clauses);
-                       verbose_stream() << " :cost " << m_cleaner.m_cleanup_counter << m_watch << ")\n";);
+                       verbose_stream() << " :cost " << m_cleaner.m_cleanup_counter << ")\n";);
         }
     };
 
